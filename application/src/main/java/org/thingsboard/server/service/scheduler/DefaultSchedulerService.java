7a46960a6141f62dec3a235bd5f7a25a61ad6fae
Andrew Shvayka <ashvayka@thingsboard.io>
2018-08-01 18:52:41 +0300

    Scheduler Service bug fix

--- application/src/main/java/org/thingsboard/server/service/scheduler/DefaultSchedulerService.java
+++ application/src/main/java/org/thingsboard/server/service/scheduler/DefaultSchedulerService.java
@@ -1,22 +1,22 @@
 /**
  * ThingsBoard, Inc. ("COMPANY") CONFIDENTIAL
- *
+ * <p>
  * Copyright © 2016-2018 ThingsBoard, Inc. All Rights Reserved.
- *
+ * <p>
  * NOTICE: All information contained herein is, and remains
  * the property of ThingsBoard, Inc. and its suppliers,
  * if any.  The intellectual and technical concepts contained
  * herein are proprietary to ThingsBoard, Inc.
  * and its suppliers and may be covered by U.S. and Foreign Patents,
  * patents in process, and are protected by trade secret or copyright law.
- *
+ * <p>
  * Dissemination of this information or reproduction of this material is strictly forbidden
  * unless prior written permission is obtained from COMPANY.
- *
+ * <p>
  * Access to the source code contained herein is hereby forbidden to anyone except current COMPANY employees,
  * managers or contractors who have executed Confidentiality and Non-disclosure agreements
  * explicitly covering such access.
- *
+ * <p>
  * The copyright notice above does not evidence any actual or intended publication
  * or disclosure  of  this source code, which includes
  * information that is confidential and/or proprietary, and is a trade secret, of  COMPANY.
@@ -236,7 +236,7 @@ public class DefaultSchedulerService implements SchedulerService {
                             TbMsgDataType.JSON, getMsgBody(event.getConfiguration()),
                             null, null, 0L);
                     actorService.onMsg(new ServiceToRuleEngineMsg(event.getTenantId(), tbMsg));
-                } catch (JsonProcessingException e) {
+                } catch (Exception e) {
                     log.error("[{}][{}] Failed to trigger event", event.getTenantId(), eventId, e);
                 }
                 scheduleNextEvent(System.currentTimeMillis(), event, md);
@@ -261,7 +261,9 @@ public class DefaultSchedulerService implements SchedulerService {
         EntityId originatorId = eventId;
         if (configuration.has("originatorId")) {
             JsonNode entityId = configuration.get("originatorId");
-            originatorId = EntityIdFactory.getByTypeAndId(entityId.get("entityType").asText(), entityId.get("id").asText());
+            if (eventId != null) {
+                originatorId = EntityIdFactory.getByTypeAndId(entityId.get("entityType").asText(), entityId.get("id").asText());
+            }
         }
         return originatorId;
     }
