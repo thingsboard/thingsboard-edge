4b277b8406bac29e1f5740d936aa27830fd863ad
Sergey Tarnavskiy <starnavskiy@thingsboard.io>
2018-10-05 14:56:44 +0300

    Forbidden to edit the type of converter after converter's creation

--- ui/src/app/converter/converter.directive.js
+++ ui/src/app/converter/converter.directive.js
@@ -143,6 +143,7 @@ export default function ConverterDirective($compile, $templateCache, $translate,
             converter: '=',
             isEdit: '=',
             theForm: '=',
+            isCreate: '<',
             onExportConverter: '&',
             onDeleteConverter: '&'
         }
