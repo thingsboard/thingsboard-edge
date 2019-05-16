80c10515ebd7cab6e4906624e6ff6bb6ea6c61d6
ShvaykaD <mitiabmx@gmail.com>
2019-05-16 19:43:22 +0300

    Revert "improvement/TbGroupActionNodes"
    This reverts commit 051eefe4d86597e44a1a213692cb7aeb920f43cf.

--- rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/action/TbAddToGroupNode.java
+++ rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/action/TbAddToGroupNode.java
@@ -30,10 +30,6 @@
  */
 package org.thingsboard.rule.engine.action;
 
-import com.google.common.util.concurrent.Futures;
-import com.google.common.util.concurrent.ListenableFuture;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.thingsboard.rule.engine.api.RuleNode;
 import org.thingsboard.rule.engine.api.TbContext;
@@ -41,26 +37,9 @@ import org.thingsboard.rule.engine.api.TbNodeConfiguration;
 import org.thingsboard.rule.engine.api.TbNodeException;
 import org.thingsboard.rule.engine.api.util.DonAsynchron;
 import org.thingsboard.rule.engine.api.util.TbNodeUtils;
-import org.thingsboard.server.common.data.Customer;
-import org.thingsboard.server.common.data.Dashboard;
-import org.thingsboard.server.common.data.Device;
-import org.thingsboard.server.common.data.EntityType;
-import org.thingsboard.server.common.data.EntityView;
-import org.thingsboard.server.common.data.asset.Asset;
-import org.thingsboard.server.common.data.group.EntityGroup;
-import org.thingsboard.server.common.data.id.AssetId;
-import org.thingsboard.server.common.data.id.CustomerId;
-import org.thingsboard.server.common.data.id.DashboardId;
-import org.thingsboard.server.common.data.id.DeviceId;
 import org.thingsboard.server.common.data.id.EntityGroupId;
-import org.thingsboard.server.common.data.id.EntityViewId;
 import org.thingsboard.server.common.data.plugin.ComponentType;
 import org.thingsboard.server.common.msg.TbMsg;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @RuleNode(
@@ -95,115 +74,24 @@ public class TbAddToGroupNode extends TbAbstractGroupActionNode<TbAddToGroupConf
     }
 
     private void removeFromCurrentGroups(TbContext ctx, TbMsg msg, EntityGroupId entityGroupId) {
-        DonAsynchron.withCallback(Futures.allAsList(getListenableFutures(ctx, msg)), containerList -> {
-            if (!containerList.isEmpty()) {
-                processRemove(ctx, msg, entityGroupId, containerList, getAllGroupId(containerList));
-            }
-        }, throwable -> {
-            throw new RuntimeException(throwable);
-        });
-
-    }
-
-    private EntityGroupId getAllGroupId(List<EntityGroupContainer> containerList) {
-        for (EntityGroupContainer group : containerList) {
-            if (group != null) {
-                if (group.isGroupAll() && !group.getEntityGroupIds().isEmpty()) {
-                    return group.getEntityGroupIds().get(0);
-                }
-            } else {
-                throw new RuntimeException("Entity Group All doesn't exist");
-            }
-        }
-        throw new RuntimeException("Entity Group All doesn't exist");
-    }
-
-    private void processRemove(TbContext ctx, TbMsg msg, EntityGroupId entityGroupId, List<EntityGroupContainer> containerList, EntityGroupId groupAllId) {
-        for (EntityGroupContainer groupContainer : containerList) {
-            if (!groupContainer.isGroupAll()) {
-                for (EntityGroupId groupId : groupContainer.getEntityGroupIds()) {
-                    if (!groupId.equals(entityGroupId) && !groupId.equals(groupAllId)) {
+        DonAsynchron.withCallback(ctx.getPeContext().getEntityGroupService()
+                .findEntityGroupsForEntity(ctx.getTenantId(), msg.getOriginator()), entityGroupIds -> {
+            if (!entityGroupIds.isEmpty()) {
+                for (EntityGroupId groupId : entityGroupIds) {
+                    if (!groupId.equals(entityGroupId)) {
                         ctx.getPeContext().getEntityGroupService()
                                 .removeEntityFromEntityGroup(ctx.getTenantId(), groupId, msg.getOriginator());
                     }
                 }
             }
-        }
-    }
-
-    private List<ListenableFuture<EntityGroupContainer>> getListenableFutures(TbContext ctx, TbMsg msg) {
-        List<ListenableFuture<EntityGroupContainer>> futures = new ArrayList<>();
-        futures.add(Futures.transform(ctx.getPeContext().getEntityGroupService()
-                .findEntityGroupsForEntity(ctx.getTenantId(), msg.getOriginator()), list -> new EntityGroupContainer(list, false), ctx.getDbCallbackExecutor()));
-        futures.add(Futures.transform(getEntityGroupAllListenableFuture(ctx, msg), entityGroupAll -> entityGroupAll
-                .map(entityGroup -> new EntityGroupContainer(Collections.singletonList(entityGroup.getId()), true))
-                .orElse(null), ctx.getDbCallbackExecutor()));
-        return futures;
-    }
-
-    private ListenableFuture<Optional<EntityGroup>> getEntityGroupAllListenableFuture(TbContext ctx, TbMsg msg) {
-        switch (msg.getOriginator().getEntityType()) {
-            case DEVICE:
-                ListenableFuture<Device> deviceListenableFuture = ctx.getDeviceService().findDeviceByIdAsync(ctx.getTenantId(), new DeviceId(msg.getOriginator().getId()));
-                return Futures.transformAsync(deviceListenableFuture, device -> {
-                    if (device != null) {
-                        return ctx.getPeContext().getEntityGroupService().findEntityGroupByTypeAndName(ctx.getTenantId(), device.getOwnerId(), EntityType.DEVICE, EntityGroup.GROUP_ALL_NAME);
-                    } else {
-                        return Futures.immediateFuture(Optional.empty());
-                    }
-                }, ctx.getDbCallbackExecutor());
-            case ASSET:
-                ListenableFuture<Asset> assetListenableFuture = ctx.getAssetService().findAssetByIdAsync(ctx.getTenantId(), new AssetId(msg.getOriginator().getId()));
-                return Futures.transformAsync(assetListenableFuture, asset -> {
-                    if (asset != null) {
-                        return ctx.getPeContext().getEntityGroupService().findEntityGroupByTypeAndName(ctx.getTenantId(), asset.getOwnerId(), EntityType.DEVICE, EntityGroup.GROUP_ALL_NAME);
-                    } else {
-                        return Futures.immediateFuture(Optional.empty());
-                    }
-                }, ctx.getDbCallbackExecutor());
-            case CUSTOMER:
-                ListenableFuture<Customer> customerListenableFuture = ctx.getCustomerService().findCustomerByIdAsync(ctx.getTenantId(), new CustomerId(msg.getOriginator().getId()));
-                return Futures.transformAsync(customerListenableFuture, customer -> {
-                    if (customer != null) {
-                        return ctx.getPeContext().getEntityGroupService().findEntityGroupByTypeAndName(ctx.getTenantId(), customer.getOwnerId(), EntityType.DEVICE, EntityGroup.GROUP_ALL_NAME);
-                    } else {
-                        return Futures.immediateFuture(Optional.empty());
-                    }
-                }, ctx.getDbCallbackExecutor());
-            case ENTITY_VIEW:
-                ListenableFuture<EntityView> entityViewListenableFuture = ctx.getEntityViewService().findEntityViewByIdAsync(ctx.getTenantId(), new EntityViewId(msg.getOriginator().getId()));
-                return Futures.transformAsync(entityViewListenableFuture, entityView -> {
-                    if (entityView != null) {
-                        return ctx.getPeContext().getEntityGroupService().findEntityGroupByTypeAndName(ctx.getTenantId(), entityView.getOwnerId(), EntityType.DEVICE, EntityGroup.GROUP_ALL_NAME);
-                    } else {
-                        return Futures.immediateFuture(Optional.empty());
-                    }
-                }, ctx.getDbCallbackExecutor());
-            case DASHBOARD:
-                ListenableFuture<Dashboard> dashboardListenableFuture = ctx.getDashboardService().findDashboardByIdAsync(ctx.getTenantId(), new DashboardId(msg.getOriginator().getId()));
-                return Futures.transformAsync(dashboardListenableFuture, dashboard -> {
-                    if (dashboard != null) {
-                        return ctx.getPeContext().getEntityGroupService().findEntityGroupByTypeAndName(ctx.getTenantId(), dashboard.getOwnerId(), EntityType.DEVICE, EntityGroup.GROUP_ALL_NAME);
-                    } else {
-                        return Futures.immediateFuture(Optional.empty());
-                    }
-                }, ctx.getDbCallbackExecutor());
-            default:
-                throw new RuntimeException("Entity with EntityType: '" + msg.getOriginator().getEntityType() + "'  doesn't support grouping");
-        }
+        }, throwable -> {
+            throw new RuntimeException(throwable);
+        });
     }
 
     private void addEntityToGroup(TbContext ctx, TbMsg msg, EntityGroupId entityGroupId) {
         ctx.getPeContext().getEntityGroupService().addEntityToEntityGroup(ctx.getTenantId(), entityGroupId, msg.getOriginator());
     }
-
-    @Data
-    @AllArgsConstructor
-    private static class EntityGroupContainer {
-
-        private List<EntityGroupId> entityGroupIds;
-        private boolean groupAll;
-
-    }
-
 }
+
+
