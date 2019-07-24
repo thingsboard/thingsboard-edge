fcaf82828b4c4fdb8ec73389594039015c477352
basanets <vbasanec@gmail.com>
2019-07-24 19:52:32 +0300

    Added headers

--- application/src/test/java/org/thingsboard/server/service/tcpip/tcp/payload/AbstractTcpHexPayloadHandlerIntegrationTest.java
+++ application/src/test/java/org/thingsboard/server/service/tcpip/tcp/payload/AbstractTcpHexPayloadHandlerIntegrationTest.java
@@ -1,3 +1,33 @@
+/**
+ * ThingsBoard, Inc. ("COMPANY") CONFIDENTIAL
+ *
+ * Copyright © 2016-2019 ThingsBoard, Inc. All Rights Reserved.
+ *
+ * NOTICE: All information contained herein is, and remains
+ * the property of ThingsBoard, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to ThingsBoard, Inc.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ *
+ * Dissemination of this information or reproduction of this material is strictly forbidden
+ * unless prior written permission is obtained from COMPANY.
+ *
+ * Access to the source code contained herein is hereby forbidden to anyone except current COMPANY employees,
+ * managers or contractors who have executed Confidentiality and Non-disclosure agreements
+ * explicitly covering such access.
+ *
+ * The copyright notice above does not evidence any actual or intended publication
+ * or disclosure  of  this source code, which includes
+ * information that is confidential and/or proprietary, and is a trade secret, of  COMPANY.
+ * ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC  PERFORMANCE,
+ * OR PUBLIC DISPLAY OF OR THROUGH USE  OF THIS  SOURCE CODE  WITHOUT
+ * THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED,
+ * AND IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES.
+ * THE RECEIPT OR POSSESSION OF THIS SOURCE CODE AND/OR RELATED INFORMATION
+ * DOES NOT CONVEY OR IMPLY ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS,
+ * OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
+ */
 package org.thingsboard.server.service.tcpip.tcp.payload;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -13,6 +43,8 @@ import org.thingsboard.server.common.data.Device;
 import org.thingsboard.server.common.data.Tenant;
 import org.thingsboard.server.common.data.integration.IntegrationType;
 import org.thingsboard.server.service.tcpip.tcp.AbstractTcpHandlerIntegrationTest;
+
+import java.io.InputStream;
 
 import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -40,8 +72,8 @@ public abstract class AbstractTcpHexPayloadHandlerIntegrationTest extends Abstra
         assertNotNull(this.tenant);
 
         loginTenantAdmin();
-        ObjectNode jsonFile = mapper.readValue(
-                ObjectNode.class.getClassLoader().getResourceAsStream(RESOURCE_FILE_NAME), ObjectNode.class);
+        InputStream resourceAsStream = ObjectNode.class.getClassLoader().getResourceAsStream(RESOURCE_FILE_NAME);
+        ObjectNode jsonFile = mapper.readValue(resourceAsStream, ObjectNode.class);
         assertNotNull(jsonFile);
         if (jsonFile.has("configuration")) {
             converter = createConverter(CONVERTER_NAME, jsonFile.get("configuration"));
