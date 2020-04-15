c67a5486cda8764372513d52767f01ce60d26e6c
Igor Kulikov <ikulikov@thingsboard.io>
2020-04-15 16:38:13 +0300

    UI: Implement Blob Entities widget. Fix Timer-based Scheduler Event presentation.

--- ui-ngx/src/app/modules/home/components/blob-entity/blob-entities.models.ts
+++ ui-ngx/src/app/modules/home/components/blob-entity/blob-entities.models.ts
@@ -29,28 +29,14 @@
 /// OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
 ///
 
-import { BaseData } from '@shared/models/base-data';
-import { TenantId } from '@shared/models/id/tenant-id';
-import { CustomerId } from '@shared/models/id/customer-id';
-import { BlobEntityId } from '@shared/models/id/blob-entity-id';
 
-export interface BlobEntityInfo extends BaseData<BlobEntityId> {
-  tenantId?: TenantId;
-  customerId?: CustomerId;
-  name: string;
-  type: string;
-  contentType: string;
-  additionalInfo?: any;
+export interface BlobEntitiesWidgetSettings {
+  title: string;
+  displayCreatedTime: boolean;
+  displayType: boolean;
+  displayCustomer: boolean;
+  displayPagination: boolean;
+  defaultPageSize: number;
+  defaultSortOrder: string;
+  forceDefaultType: string;
 }
-
-export interface BlobEntityWithCustomerInfo extends BlobEntityInfo {
-  customerTitle: string;
-  customerIsPublic: boolean;
-  typeName?: string;
-}
-
-export const blobEntityTypeTranslationMap = new Map<string, string>(
-  [
-    ['report', 'blob-entity.report']
-  ]
-);
