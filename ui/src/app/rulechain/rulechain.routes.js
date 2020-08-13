48f89a0c108573025d13e9a260cd940b89860491
deaflynx <artemio.babak@gmail.com>
2020-08-13 15:09:02 +0300

    Minor fix rulechain routes

--- ui/src/app/rulechain/rulechain.routes.js
+++ ui/src/app/rulechain/rulechain.routes.js
@@ -208,6 +208,7 @@ export default function RuleChainRoutes($stateProvider, NodeTemplatePathProvider
         }
     }).state('home.edgeGroups.edgeGroup.ruleChains', {
         url: '/:edgeId/ruleChains',
+        params: {'topIndex': 0},
         module: 'private',
         auth: ['TENANT_ADMIN'],
         views: {
