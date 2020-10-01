2610e2d8e0737961b9cf63487d2ef587313320fe
ShvaykaD <dshvaika@thingsboard.io>
2020-10-01 17:55:38 +0300

    fixed change user owner logic from tenant to customer and back

--- application/src/main/java/org/thingsboard/server/service/security/permission/DefaultOwnersCacheService.java
+++ application/src/main/java/org/thingsboard/server/service/security/permission/DefaultOwnersCacheService.java
@@ -177,7 +177,15 @@ public class DefaultOwnersCacheService implements OwnersCacheService {
     @Override
     public void changeUserOwner(TenantId tenantId, EntityId targetOwnerId, User user) throws ThingsboardException {
         userPermissionsService.onUserUpdatedOrRemoved(user);
-        changeEntityOwner(tenantId, targetOwnerId, user.getId(), user, userService::saveUser);
+        changeEntityOwner(tenantId, targetOwnerId, user.getId(), user, targetUser -> {
+            if (EntityType.CUSTOMER.equals(targetOwnerId.getEntityType())) {
+                targetUser.setAuthority(Authority.CUSTOMER_USER);
+            }
+            if (EntityType.TENANT.equals(targetOwnerId.getEntityType())) {
+                targetUser.setAuthority(Authority.TENANT_ADMIN);
+            }
+            userService.saveUser(targetUser);
+        });
     }
 
     @Override
