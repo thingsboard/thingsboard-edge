dc877883ebcf389513e272964a4e49e070653952
Valerii Sosliuk <valerii@thingsboard.io>
2020-10-14 16:55:29 +0300

    Skip recursive tag scan. Added explicit disconnect at the end of the scan

--- common/integration/opcua-integrations/src/main/java/org/thingsboard/integration/opcua/OpcUaIntegration.java
+++ common/integration/opcua-integrations/src/main/java/org/thingsboard/integration/opcua/OpcUaIntegration.java
@@ -34,6 +34,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
 import org.eclipse.milo.opcua.sdk.client.api.config.OpcUaClientConfig;
 import org.eclipse.milo.opcua.sdk.client.api.config.OpcUaClientConfigBuilder;
@@ -306,6 +307,7 @@ public class OpcUaIntegration extends AbstractIntegration<OpcUaIntegrationMsg> {
                 t = e.getCause();
             }
             log.error("[{}] Failed to connect to OPC-UA server. Reason: {}", this.configuration.getName(), t.getMessage(), t);
+            disconnect();
             throw new OpcUaIntegrationException("Failed to connect to OPC-UA server. Reason: " + t.getMessage(), e);
         }
     }
@@ -414,6 +416,7 @@ public class OpcUaIntegration extends AbstractIntegration<OpcUaIntegrationMsg> {
                 log.debug("[{}] Scheduling next scan in {} seconds!", this.configuration.getId(), opcUaServerConfiguration.getScanPeriodInSeconds());
                 scheduleScan();
             }
+            disconnect();
         } catch (Throwable e) {
             log.warn("[{}] Periodic device scan failed!", this.configuration.getId(), e);
             scheduleReconnect = true;
@@ -653,6 +656,12 @@ public class OpcUaIntegration extends AbstractIntegration<OpcUaIntegrationMsg> {
                 } else {
                     name = rd.getBrowseName().getName();
                 }
+
+                if (StringUtils.isEmpty(name) || name.endsWith("//")) {
+                    log.trace("[{}] Ignoring self-referenced node: {}", this.configuration.getName(), rd.getNodeId());
+                    continue;
+                }
+
                 log.trace("[{}] Found tag: [{}].[{}]", this.configuration.getName(), nodeId, name);
                 if (tags.contains(name)) {
                     values.put(name, childId);
