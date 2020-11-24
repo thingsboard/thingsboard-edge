9ee404ef189049ceab6e21f578a44dedabe1f2eb
Artem Babak <artemio.babak@gmail.com>
2020-11-24 17:04:56 +0200

    Added pages/edge/ files

---
+++ ui-ngx/src/app/modules/home/pages/edge/edge-group-config.factory.ts
@@ -0,0 +1,246 @@
+///
+/// ThingsBoard, Inc. ("COMPANY") CONFIDENTIAL
+///
+/// Copyright © 2016-2020 ThingsBoard, Inc. All Rights Reserved.
+///
+/// NOTICE: All information contained herein is, and remains
+/// the property of ThingsBoard, Inc. and its suppliers,
+/// if any.  The intellectual and technical concepts contained
+/// herein are proprietary to ThingsBoard, Inc.
+/// and its suppliers and may be covered by U.S. and Foreign Patents,
+/// patents in process, and are protected by trade secret or copyright law.
+///
+/// Dissemination of this information or reproduction of this material is strictly forbidden
+/// unless prior written permission is obtained from COMPANY.
+///
+/// Access to the source code contained herein is hereby forbidden to anyone except current COMPANY employees,
+/// managers or contractors who have executed Confidentiality and Non-disclosure agreements
+/// explicitly covering such access.
+///
+/// The copyright notice above does not evidence any actual or intended publication
+/// or disclosure  of  this source code, which includes
+/// information that is confidential and/or proprietary, and is a trade secret, of  COMPANY.
+/// ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC  PERFORMANCE,
+/// OR PUBLIC DISPLAY OF OR THROUGH USE  OF THIS  SOURCE CODE  WITHOUT
+/// THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED,
+/// AND IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES.
+/// THE RECEIPT OR POSSESSION OF THIS SOURCE CODE AND/OR RELATED INFORMATION
+/// DOES NOT CONVEY OR IMPLY ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS,
+/// OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
+///
+
+import {Observable, of} from 'rxjs';
+import {TranslateService} from '@ngx-translate/core';
+import {UtilsService} from '@core/services/utils.service';
+import {
+  EntityGroupStateConfigFactory,
+  EntityGroupStateInfo,
+  GroupEntityTableConfig
+} from '@home/models/group/group-entities-table-config.models';
+import {Injectable} from '@angular/core';
+import {EntityType} from '@shared/models/entity-type.models';
+import {tap} from 'rxjs/operators';
+import {BroadcastService} from '@core/services/broadcast.service';
+import {EntityAction} from '@home/models/entity/entity-component.models';
+import {MatDialog} from '@angular/material/dialog';
+import {UserPermissionsService} from '@core/http/user-permissions.service';
+import {EntityGroupParams, ShortEntityView} from '@shared/models/entity-group.models';
+import {HomeDialogsService} from '@home/dialogs/home-dialogs.service';
+import {CustomerId} from '@shared/models/id/customer-id';
+import {GroupConfigTableConfigService} from '@home/components/group/group-config-table-config.service';
+import {Operation, Resource} from '@shared/models/security.models';
+import {Edge} from "@shared/models/edge.models";
+import {EdgeService} from "@core/http/edge.service";
+import {EdgeComponent} from "@home/pages/edge/edge.component";
+import {Router} from "@angular/router";
+
+@Injectable()
+export class EdgeGroupConfigFactory implements EntityGroupStateConfigFactory<Edge> {
+
+  constructor(private groupConfigTableConfigService: GroupConfigTableConfigService<Edge>,
+              private userPermissionsService: UserPermissionsService,
+              private translate: TranslateService,
+              private utils: UtilsService,
+              private dialog: MatDialog,
+              private homeDialogs: HomeDialogsService,
+              private edgeService: EdgeService,
+              private broadcast: BroadcastService,
+              private router: Router) {
+  }
+
+  createConfig(params: EntityGroupParams, entityGroup: EntityGroupStateInfo<Edge>): Observable<GroupEntityTableConfig<Edge>> {
+    const config = new GroupEntityTableConfig<Edge>(entityGroup, params);
+
+    config.entityComponent = EdgeComponent;
+
+    config.entityTitle = (edge) => edge ?
+      this.utils.customTranslation(edge.name, edge.name) : '';
+
+    config.deleteEntityTitle = edge => this.translate.instant('edge.delete-edge-title', { edgeName: edge.name });
+    config.deleteEntityContent = () => this.translate.instant('edge.delete-edge-text');
+    config.deleteEntitiesTitle = count => this.translate.instant('edge.delete-edges-title', {count});
+    config.deleteEntitiesContent = () => this.translate.instant('edge.delete-edges-text');
+
+    config.loadEntity = id => this.edgeService.getEdge(id.id);
+    config.saveEntity = edge => {
+      return this.edgeService.saveEdge(edge).pipe(
+        tap(() => {
+          this.broadcast.broadcast('edgeSaved');
+        }));
+    };
+    config.deleteEntity = id => this.edgeService.deleteEdge(id.id);
+
+    config.onEntityAction = action => this.onEdgeAction(action, config, params);
+
+    if (this.userPermissionsService.hasGroupEntityPermission(Operation.CREATE, config.entityGroup)) {
+      config.headerActionDescriptors.push(
+        {
+          name: this.translate.instant('edge.import'),
+          icon: 'file_upload',
+          isEnabled: () => true,
+          onAction: ($event) => this.importEdges($event, config)
+        }
+      );
+    }
+
+    if (this.userPermissionsService.hasGenericPermission(Resource.EDGE, Operation.READ)) {
+      config.cellActionDescriptors.push(
+        {
+          name: this.translate.instant('edge.manage-edge-asset-groups'),
+          icon: 'domain',
+          isEnabled: config.manageAssetsEnabled,
+          onAction: ($event, entity) => this.manageAssets($event, entity, config, params)
+        },
+        {
+          name: this.translate.instant('edge.manage-edge-device-groups'),
+          icon: 'devices_other',
+          isEnabled: config.manageDevicesEnabled,
+          onAction: ($event, entity) => this.manageDevices($event, entity, config, params)
+        },
+        {
+          name: this.translate.instant('edge.manage-edge-entity-view-groups'),
+          icon: 'view_quilt',
+          isEnabled: config.manageEntityViewsEnabled,
+          onAction: ($event, entity) => this.manageEntityViews($event, entity, config, params)
+        },
+        {
+          name: this.translate.instant('edge.manage-edge-dashboard-groups'),
+          icon: 'dashboard',
+          isEnabled: config.manageAssetsEnabled,
+          onAction: ($event, entity) => this.manageDashboards($event, entity, config, params)
+        },
+        {
+          name: this.translate.instant('edge.manage-edge-scheduler-events'),
+          icon: 'schedule',
+          isEnabled: config.manageAssetsEnabled,
+          onAction: ($event, entity) => this.manageAssets($event, entity, config, params)
+        },
+        {
+          name: this.translate.instant('edge.manage-edge-rule-chains'),
+          icon: 'settings_ethernet',
+          isEnabled: config.manageAssetsEnabled,
+          onAction: ($event, entity) => this.manageRuleChains($event, entity, config, params)
+        }
+      )
+    }
+
+    return of(this.groupConfigTableConfigService.prepareConfiguration(params, config));
+  }
+
+  importEdges($event: Event, config: GroupEntityTableConfig<Edge>) {
+    const entityGroup = config.entityGroup;
+    const entityGroupId = !entityGroup.groupAll ? entityGroup.id.id : null;
+    let customerId: CustomerId = null;
+    if (entityGroup.ownerId.entityType === EntityType.CUSTOMER) {
+      customerId = entityGroup.ownerId as CustomerId;
+    }
+    this.homeDialogs.importEntities(customerId, EntityType.EDGE, entityGroupId).subscribe((res) => {
+      if (res) {
+        this.broadcast.broadcast('edgeSaved');
+        config.table.updateData();
+      }
+    });
+  }
+
+  onEdgeAction(action: EntityAction<Edge>, config: GroupEntityTableConfig<Edge>, params: EntityGroupParams): boolean {
+    switch (action.action) {
+      case 'manageAssets':
+        this.manageAssets(action.event, action.entity, config, params);
+        return true;
+      case 'manageDevices':
+        this.manageDevices(action.event, action.entity, config, params);
+        return true;
+      case 'manageEntityViews':
+        this.manageEntityViews(action.event, action.entity, config, params);
+        return true;
+      case 'manageDashboards':
+        this.manageDashboards(action.event, action.entity, config, params);
+        return true;
+      case 'manageRuleChains':
+        this.manageRuleChains(action.event, action.entity, config, params);
+        return true;
+    }
+    return false;
+  }
+
+  manageAssets($event: Event, edge: Edge | ShortEntityView, config: GroupEntityTableConfig<Edge>,
+               params: EntityGroupParams) {
+    if ($event) {
+      $event.stopPropagation();
+    }
+    if (params.hierarchyView) {
+      params.hierarchyCallbacks.edgeGroupsSelected(params.nodeId, edge.id.id, EntityType.EDGE);
+    } else {
+      this.router.navigateByUrl(`edgeGroups/${config.entityGroup.id.id}/${edge.id.id}/assetGroups`);
+    }
+  }
+
+  manageDevices($event: Event, edge: Edge | ShortEntityView, config: GroupEntityTableConfig<Edge>,
+                params: EntityGroupParams) {
+    if ($event) {
+      $event.stopPropagation();
+    }
+    if (params.hierarchyView) {
+      params.hierarchyCallbacks.edgeGroupsSelected(params.nodeId, edge.id.id, EntityType.EDGE);
+    } else {
+      this.router.navigateByUrl(`edgeGroups/${config.entityGroup.id.id}/${edge.id.id}/assetGroups`);
+    }
+  }
+
+  manageEntityViews($event: Event, edge: Edge | ShortEntityView, config: GroupEntityTableConfig<Edge>,
+                    params: EntityGroupParams) {
+    if ($event) {
+      $event.stopPropagation();
+    }
+    if (params.hierarchyView) {
+      params.hierarchyCallbacks.edgeGroupsSelected(params.nodeId, edge.id.id, EntityType.EDGE);
+    } else {
+      this.router.navigateByUrl(`edgeGroups/${config.entityGroup.id.id}/${edge.id.id}/entityViewGroups`);
+    }
+  }
+
+  manageDashboards($event: Event, edge: Edge | ShortEntityView, config: GroupEntityTableConfig<Edge>,
+                   params: EntityGroupParams) {
+    if ($event) {
+      $event.stopPropagation();
+    }
+    if (params.hierarchyView) {
+      params.hierarchyCallbacks.edgeGroupsSelected(params.nodeId, edge.id.id, EntityType.EDGE);
+    } else {
+      this.router.navigateByUrl(`edgeGroups/${config.entityGroup.id.id}/${edge.id.id}/dashboardGroups`);
+    }
+  }
+
+  manageRuleChains($event: Event, edge: Edge | ShortEntityView, config: GroupEntityTableConfig<Edge>,
+                   params: EntityGroupParams) {
+    if ($event) {
+      $event.stopPropagation();
+    }
+    if (params.hierarchyView) {
+      params.hierarchyCallbacks.edgeGroupsSelected(params.nodeId, edge.id.id, EntityType.EDGE);
+    } else {
+      this.router.navigateByUrl(`edgeGroups/${config.entityGroup.id.id}/${edge.id.id}/ruleChains`);
+    }
+  }
+
+}
---
+++ ui-ngx/src/app/modules/home/pages/edge/edge.component.ts
@@ -0,0 +1,165 @@
+///
+/// ThingsBoard, Inc. ("COMPANY") CONFIDENTIAL
+///
+/// Copyright © 2016-2020 ThingsBoard, Inc. All Rights Reserved.
+///
+/// NOTICE: All information contained herein is, and remains
+/// the property of ThingsBoard, Inc. and its suppliers,
+/// if any.  The intellectual and technical concepts contained
+/// herein are proprietary to ThingsBoard, Inc.
+/// and its suppliers and may be covered by U.S. and Foreign Patents,
+/// patents in process, and are protected by trade secret or copyright law.
+///
+/// Dissemination of this information or reproduction of this material is strictly forbidden
+/// unless prior written permission is obtained from COMPANY.
+///
+/// Access to the source code contained herein is hereby forbidden to anyone except current COMPANY employees,
+/// managers or contractors who have executed Confidentiality and Non-disclosure agreements
+/// explicitly covering such access.
+///
+/// The copyright notice above does not evidence any actual or intended publication
+/// or disclosure  of  this source code, which includes
+/// information that is confidential and/or proprietary, and is a trade secret, of  COMPANY.
+/// ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC  PERFORMANCE,
+/// OR PUBLIC DISPLAY OF OR THROUGH USE  OF THIS  SOURCE CODE  WITHOUT
+/// THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED,
+/// AND IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES.
+/// THE RECEIPT OR POSSESSION OF THIS SOURCE CODE AND/OR RELATED INFORMATION
+/// DOES NOT CONVEY OR IMPLY ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS,
+/// OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
+///
+
+import { Component, Inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from "@core/core.state";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { EntityType } from "@shared/models/entity-type.models";
+import { Edge } from "@shared/models/edge.models";
+import { TranslateService } from "@ngx-translate/core";
+import { ActionNotificationShow } from "@core/notification/notification.actions";
+import { guid, isUndefined } from "@core/utils";
+import { GroupEntityTableConfig } from "@home/models/group/group-entities-table-config.models";
+import { WINDOW } from "@core/services/window.service";
+import { GroupEntityComponent } from "@home/components/group/group-entity.component";
+
+@Component({
+  selector: 'tb-edge',
+  templateUrl: './edge.component.html',
+  styleUrls: ['./edge.component.scss']
+})
+export class EdgeComponent extends GroupEntityComponent<Edge> {
+
+  entityType = EntityType;
+
+  // edgeScope: 'tenant' | 'customer' | 'customer_user';
+
+  constructor(protected store: Store<AppState>,
+              protected translate: TranslateService,
+              @Inject('entity') protected entityValue: Edge,
+              @Inject('entitiesTableConfig') protected entitiesTableConfigValue: GroupEntityTableConfig<Edge>,
+              protected fb: FormBuilder,
+              @Inject(WINDOW) protected window: Window) {
+    super(store, fb, entityValue, entitiesTableConfigValue);
+  }
+
+  ngOnInit() {
+    // this.edgeScope = this.entitiesTableConfig.componentsData.edgeScope;
+    super.ngOnInit();
+  }
+
+  hideDelete() {
+    if (this.entitiesTableConfig) {
+      return !this.entitiesTableConfig.deleteEnabled(this.entity);
+    } else {
+      return false;
+    }
+  }
+
+  hideAssignmentActions() {
+    if (this.entitiesTableConfig) {
+      return !this.entitiesTableConfig.assignmentEnabled(this.entity);
+    } else {
+      return false;
+    }
+  }
+
+  /* isAssignedToCustomer(entity: EdgeInfo): boolean {
+    return entity && entity.customerId && entity.customerId.id !== NULL_UUID;
+  } */
+
+  buildForm(entity: Edge): FormGroup {
+    return this.fb.group(
+      {
+        name: [entity ? entity.name : '', [Validators.required]],
+        type: [entity ? entity.type : null, [Validators.required]],
+        label: [entity ? entity.label : ''],
+        cloudEndpoint: [this.window.location.origin, [Validators.required]],
+        edgeLicenseKey: ['', [Validators.required]],
+        routingKey: guid(),
+        secret: this.generateSecret(20),
+        additionalInfo: this.fb.group(
+          {
+            description: [entity && entity.additionalInfo ? entity.additionalInfo.description : '']
+          }
+        )
+      }
+    );
+  }
+
+  updateForm(entity: Edge) {
+    this.entityForm.patchValue({name: entity.name});
+    this.entityForm.patchValue({type: entity.type});
+    this.entityForm.patchValue({label: entity.label});
+    this.entityForm.patchValue({cloudEndpoint: entity.cloudEndpoint});
+    this.entityForm.patchValue({edgeLicenseKey: entity.edgeLicenseKey});
+    this.entityForm.patchValue({routingKey: entity.routingKey});
+    this.entityForm.patchValue({secret: entity.secret});
+    this.entityForm.patchValue({additionalInfo: {
+      description: entity.additionalInfo ? entity.additionalInfo.description : ''}});
+  }
+
+  onEdgeIdCopied($event) {
+    this.store.dispatch(new ActionNotificationShow(
+      {
+        message: this.translate.instant('edge.id-copied-message'),
+        type: 'success',
+        duration: 750,
+        verticalPosition: 'bottom',
+        horizontalPosition: 'left'
+      }));
+  }
+
+  onEdgeKeyCopied($event) {
+    this.store.dispatch(new ActionNotificationShow(
+      {
+        message: this.translate.instant('edge.edge-key-copied-message'),
+        type: 'success',
+        duration: 750,
+        verticalPosition: 'bottom',
+        horizontalPosition: 'left'
+      }));
+  }
+
+  onEdgeSecretCopied($event) {
+    this.store.dispatch(new ActionNotificationShow(
+      {
+        message: this.translate.instant('edge.edge-secret-copied-message'),
+        type: 'success',
+        duration: 750,
+        verticalPosition: 'bottom',
+        horizontalPosition: 'left'
+      }));
+  }
+
+  generateSecret(length): string {
+    if (isUndefined(length) || length == null) {
+      length = 1;
+    }
+    var l = length > 10 ? 10 : length;
+    var str =  Math.random().toString(36).substr(2, l);
+    if (str.length >= length) {
+      return str;
+    }
+    return str.concat(this.generateSecret(length - str.length));
+  }
+}
