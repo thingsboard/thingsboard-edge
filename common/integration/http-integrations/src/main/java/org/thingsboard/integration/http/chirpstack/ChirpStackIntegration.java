711890b999b8e29d488e33d5cf92dcce6a4f9efb
zbeacon <ibarkov.ua@gmail.com>
2021-09-17 18:12:05 +0300

    Fix for downlink in Chirpstack

--- common/integration/http-integrations/src/main/java/org/thingsboard/integration/http/chirpstack/ChirpStackIntegration.java
+++ common/integration/http-integrations/src/main/java/org/thingsboard/integration/http/chirpstack/ChirpStackIntegration.java
@@ -152,10 +152,12 @@ public class ChirpStackIntegration extends BasicHttpIntegration<JsonHttpIntegrat
                     }
                     String payload = new String(downlink.getData(), StandardCharsets.UTF_8);
                     ObjectNode body = mapper.createObjectNode();
-                    body.with(DEVICE_DOWNLINK_QUEUE).put(CONFIRMED, metadata.containsKey(CONFIRMED));
+                    if (metadata.containsKey(CONFIRMED)) {
+                        body.with(DEVICE_DOWNLINK_QUEUE).put(CONFIRMED, metadata.get(CONFIRMED));
+                    }
                     body.with(DEVICE_DOWNLINK_QUEUE).put(DATA, payload);
-                    body.with(DEVICE_DOWNLINK_QUEUE).put(F_PORT, metadata.containsKey(F_PORT));
-                    httpClient.postForEntity(devicesUrl + "/" + metadata.containsKey(DEV_EUI) + "/queue", createRequest(body), String.class);
+                    body.with(DEVICE_DOWNLINK_QUEUE).put(F_PORT, metadata.get(F_PORT));
+                    httpClient.postForEntity(devicesUrl + "/" + metadata.get(DEV_EUI) + "/queue", createRequest(body), String.class);
                     reportDownlinkOk(context, downlink);
                 }
             }
