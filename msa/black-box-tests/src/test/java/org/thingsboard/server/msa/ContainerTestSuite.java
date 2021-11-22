096dbac1837ffdc6f01a39e0610577c19fd55e43
Volodymyr Babak <volodymyr.babak@gmail.com>
2021-11-22 13:19:48 +0200

    Removed unused advanced config replace name

--- msa/black-box-tests/src/test/java/org/thingsboard/server/msa/ContainerTestSuite.java
+++ msa/black-box-tests/src/test/java/org/thingsboard/server/msa/ContainerTestSuite.java
@@ -83,7 +83,6 @@ public class ContainerTestSuite {
                 final String targetDir = FileUtils.getTempDirectoryPath() + "/" + "ContainerTestSuite-" + UUID.randomUUID() + "/";
                 log.info("targetDir {}", targetDir);
                 FileUtils.copyDirectory(new File(SOURCE_DIR), new File(targetDir));
-                replaceInFile(targetDir + "advanced/docker-compose.yml", "    container_name: \"${LOAD_BALANCER_NAME}\"", "", "container_name");
 
                 class DockerComposeContainerImpl<SELF extends DockerComposeContainer<SELF>> extends DockerComposeContainer<SELF> {
                     public DockerComposeContainerImpl(File... composeFiles) {
@@ -123,30 +122,6 @@ public class ContainerTestSuite {
             FileUtils.deleteDirectory(new File(targetDir));
         } catch (IOException e) {
             log.error("Can't delete temp directory " + targetDir, e);
-        }
-    }
-
-    /**
-     * This workaround is actual until issue will be resolved:
-     * Support container_name in docker-compose file #2472 https://github.com/testcontainers/testcontainers-java/issues/2472
-     * docker-compose files which contain container_name are not supported and the creation of DockerComposeContainer fails due to IllegalStateException.
-     * This has been introduced in #1151 as a quick fix for unintuitive feedback. https://github.com/testcontainers/testcontainers-java/issues/1151
-     * Using the latest testcontainers and waiting for the fix...
-     * */
-    private static void replaceInFile(String sourceFilename, String target, String replacement, String verifyPhrase) {
-        try {
-            File file = new File(sourceFilename);
-            String sourceContent = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
-
-            String outputContent = sourceContent.replace(target, replacement);
-            assertThat(outputContent, (not(containsString(target))));
-            assertThat(outputContent, (not(containsString(verifyPhrase))));
-
-            FileUtils.writeStringToFile(file, outputContent, StandardCharsets.UTF_8);
-            assertThat(FileUtils.readFileToString(file, StandardCharsets.UTF_8), is(outputContent));
-        } catch (IOException e) {
-            log.error("failed to update file " + sourceFilename, e);
-            fail("failed to update file");
         }
     }
 }
