4c63a19ef64c7c9cce4d6748b17235c7b6431566
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2021-12-29 19:07:15 +0200

    UI: Fixed show edit button in defaultDashboard without fullscreen mode

--- ui-ngx/src/app/modules/home/pages/group/entity-group-routing.module.ts
+++ ui-ngx/src/app/modules/home/pages/group/entity-group-routing.module.ts
@@ -880,7 +880,8 @@ const routes: Routes = [
           userPermissionsService.hasResourcesGenericPermission([Resource.WIDGETS_BUNDLE, Resource.WIDGET_TYPE], Operation.READ);
       },
       title: 'dashboard.dashboard',
-      widgetEditMode: false
+      widgetEditMode: false,
+      singlePageMode: true
     },
     resolve: {
       dashboard: DashboardResolver,
