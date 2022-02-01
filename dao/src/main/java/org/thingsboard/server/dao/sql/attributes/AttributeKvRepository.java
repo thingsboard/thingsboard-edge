e41f0151ac60697b3ac950111545ca3166a15785
van-vanich <panahno13@gmail.com>
2022-02-01 18:08:38 +0200

    changed forgotten classes with CrudRepository to JpaRepository and remove unnecessary import

--- dao/src/main/java/org/thingsboard/server/dao/sql/attributes/AttributeKvRepository.java
+++ dao/src/main/java/org/thingsboard/server/dao/sql/attributes/AttributeKvRepository.java
@@ -30,9 +30,9 @@
  */
 package org.thingsboard.server.dao.sql.attributes;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import org.thingsboard.server.common.data.EntityType;
@@ -42,7 +42,7 @@ import org.thingsboard.server.dao.model.sql.AttributeKvEntity;
 import java.util.List;
 import java.util.UUID;
 
-public interface AttributeKvRepository extends CrudRepository<AttributeKvEntity, AttributeKvCompositeKey> {
+public interface AttributeKvRepository extends JpaRepository<AttributeKvEntity, AttributeKvCompositeKey> {
 
     @Query("SELECT a FROM AttributeKvEntity a WHERE a.id.entityType = :entityType " +
             "AND a.id.entityId = :entityId " +
