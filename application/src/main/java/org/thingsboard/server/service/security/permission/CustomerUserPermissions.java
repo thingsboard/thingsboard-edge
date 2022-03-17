6a877f26429c9b8a48395dc03082886cc239e4bf
Andrii Shvaika <ashvayka@thingsboard.io>
2022-03-17 18:38:20 +0200

    Critical alarm security checker fix

--- application/src/main/java/org/thingsboard/server/service/security/permission/CustomerUserPermissions.java
+++ application/src/main/java/org/thingsboard/server/service/security/permission/CustomerUserPermissions.java
@@ -94,6 +94,12 @@ public class CustomerUserPermissions extends AbstractPermissions {
     }
 
     private final PermissionChecker customerAlarmPermissionChecker = new PermissionChecker() {
+
+        @Override
+        public boolean hasPermission(SecurityUser user, Resource resource, Operation operation) {
+            return user.getUserPermissions().hasGenericPermission(resource, operation);
+        }
+
         @Override
         public boolean hasPermission(SecurityUser user, Operation operation, EntityId entityId, TenantEntity entity) throws ThingsboardException {
             if (!user.getTenantId().equals(entity.getTenantId())) {
