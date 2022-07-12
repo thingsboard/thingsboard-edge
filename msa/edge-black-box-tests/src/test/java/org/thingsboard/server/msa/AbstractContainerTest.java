35e58d592f7bbd5b36a27a53ba7744e2966926ed
Volodymyr Babak <volodymyr.babak@gmail.com>
2022-07-12 22:49:42 +0300

    Fixed bbt compilation error

--- msa/edge-black-box-tests/src/test/java/org/thingsboard/server/msa/AbstractContainerTest.java
+++ msa/edge-black-box-tests/src/test/java/org/thingsboard/server/msa/AbstractContainerTest.java
@@ -57,7 +57,6 @@ import org.thingsboard.server.common.data.device.profile.DeviceProfileData;
 import org.thingsboard.server.common.data.device.profile.DeviceProfileTransportConfiguration;
 import org.thingsboard.server.common.data.device.profile.SimpleAlarmConditionSpec;
 import org.thingsboard.server.common.data.edge.Edge;
-import org.thingsboard.server.common.data.id.QueueId;
 import org.thingsboard.server.common.data.id.RuleChainId;
 import org.thingsboard.server.common.data.page.PageData;
 import org.thingsboard.server.common.data.page.PageLink;
@@ -95,8 +94,6 @@ public abstract class AbstractContainerTest {
     protected static Edge edge;
     protected static String edgeUrl;
 
-    protected static QueueId defaultQueueId;
-
     @BeforeClass
     public static void before() throws Exception {
         cloudRestClient = new RestClient(CLOUD_HTTPS_URL);
@@ -126,8 +123,6 @@ public abstract class AbstractContainerTest {
                     return loginSuccessful;
                 });
 
-        defaultQueueId = getRandomQueueId();
-
         Optional<Tenant> tenant = edgeRestClient.getTenantById(edge.getTenantId());
         Assert.assertTrue(tenant.isPresent());
         Assert.assertEquals(edge.getTenantId(), tenant.get().getId());
@@ -138,14 +133,6 @@ public abstract class AbstractContainerTest {
 
         // This is a starting point to start other tests
         verifyWidgetBundles();
-    }
-
-    private static QueueId getRandomQueueId() {
-        return cloudRestClient
-                .getQueuesByServiceType("TB_RULE_ENGINE", new PageLink(100))
-                .getData()
-                .get(0)
-                .getId();
     }
 
     private static void verifyWidgetBundles() {
@@ -254,7 +241,7 @@ public abstract class AbstractContainerTest {
         deviceProfile.setProfileData(deviceProfileData);
         deviceProfile.setDefault(false);
         deviceProfile.setDefaultRuleChainId(null);
-        deviceProfile.setDefaultQueueId(defaultQueueId);
+        deviceProfile.setDefaultQueueName("Main");
         extendDeviceProfileData(deviceProfile);
         return cloudRestClient.saveDeviceProfile(deviceProfile);
     }
