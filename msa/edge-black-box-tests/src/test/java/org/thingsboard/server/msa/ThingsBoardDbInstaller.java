2a496e4350505e033f01f7a6812315febf8b204b
Volodymyr Babak <volodymyr.babak@gmail.com>
2022-09-02 11:48:01 +0300

    Set version to 3.4.2EDGE-SNAPSHOT

--- msa/edge-black-box-tests/src/test/java/org/thingsboard/server/msa/ThingsBoardDbInstaller.java
+++ msa/edge-black-box-tests/src/test/java/org/thingsboard/server/msa/ThingsBoardDbInstaller.java
@@ -76,7 +76,7 @@ public class ThingsBoardDbInstaller extends ExternalResource {
 
             env.put("EDGE_DOCKER_REPO", "thingsboard");
             env.put("TB_EDGE_DOCKER_NAME", "tb-edge");
-            env.put("TB_EDGE_VERSION", "3.4.1EDGE-SNAPSHOT");
+            env.put("TB_EDGE_VERSION", "3.4.2EDGE-SNAPSHOT");
 
             dockerCompose.withEnv(env);
         } catch (Exception e) {
