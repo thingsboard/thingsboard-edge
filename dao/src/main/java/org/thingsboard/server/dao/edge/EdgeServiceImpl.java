648fb96c4bb11c0f835d186c1293d1c3336d4858
Volodymyr Babak <volodymyr.babak@gmail.com>
2022-10-06 19:00:13 +0300

    Fixed fetch of related edges for customer entity

--- dao/src/main/java/org/thingsboard/server/dao/edge/EdgeServiceImpl.java
+++ dao/src/main/java/org/thingsboard/server/dao/edge/EdgeServiceImpl.java
@@ -424,8 +424,9 @@ public class EdgeServiceImpl extends AbstractCachedEntityService<EdgeCacheKey, E
             case TENANT:
             case DEVICE_PROFILE:
             case OTA_PACKAGE:
-            case CUSTOMER:
                 return convertToEdgeIds(findEdgesByTenantId(tenantId, pageLink));
+            case CUSTOMER:
+                return convertToEdgeIds(findEdgesByTenantIdAndCustomerId(tenantId, new CustomerId(entityId.getId()), pageLink));
             case EDGE:
                 List<EdgeId> edgeIds = Collections.singletonList(new EdgeId(entityId.getId()));
                 return new PageData<>(edgeIds, 1, 1, false);
