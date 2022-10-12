32b43326bc7f8d19d1d29010f34b1c33b64e171a
Volodymyr Babak <volodymyr.babak@gmail.com>
2022-10-12 13:27:07 +0300

    Do not create asset profile on edge

--- dao/src/main/java/org/thingsboard/server/dao/device/DeviceServiceImpl.java
+++ dao/src/main/java/org/thingsboard/server/dao/device/DeviceServiceImpl.java
@@ -215,14 +215,12 @@ public class DeviceServiceImpl extends AbstractCachedEntityService<DeviceCacheKe
             DeviceProfile deviceProfile;
             if (device.getDeviceProfileId() == null) {
                 if (!StringUtils.isEmpty(device.getType())) {
-
                     // TODO: @voba device profiles are not created on edge at the moment
                     deviceProfile = this.deviceProfileService.findDeviceProfileByName(device.getTenantId(), device.getType());
                     if (deviceProfile == null) {
                         deviceProfile = this.deviceProfileService.findDefaultDeviceProfile(device.getTenantId());
                     }
                     // deviceProfile = this.deviceProfileService.findOrCreateDeviceProfile(device.getTenantId(), device.getType());
-
                 } else {
                     deviceProfile = this.deviceProfileService.findDefaultDeviceProfile(device.getTenantId());
                 }
