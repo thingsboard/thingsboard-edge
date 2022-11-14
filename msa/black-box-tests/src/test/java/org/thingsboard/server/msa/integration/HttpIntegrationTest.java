e353943ca91229207601da579f3d20549055fdbc
dashevchenko <dshevchenko@thingsboard.io>
2022-11-14 11:57:38 +0200

    disabled test

--- msa/black-box-tests/src/test/java/org/thingsboard/server/msa/integration/HttpIntegrationTest.java
+++ msa/black-box-tests/src/test/java/org/thingsboard/server/msa/integration/HttpIntegrationTest.java
@@ -244,7 +244,7 @@ public class HttpIntegrationTest extends AbstractContainerTest {
                 .until(() -> wsClient.getMessage().getDataValuesByKey(TELEMETRY_KEY).get(1).equals(temperatureValue3));
     }
 
-    @Test
+    @Test(enabled = false)
     public void checkTelemetryUploadedAfterRemoteIntegrationConfigUpdated() throws Exception {
         JsonNode config = defaultConfigWithSecurityHeader(HTTPS_URL);
         integration = Integration.builder()
