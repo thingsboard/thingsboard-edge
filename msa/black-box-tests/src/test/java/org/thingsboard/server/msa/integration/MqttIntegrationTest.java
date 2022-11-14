6f6a839dc4a910b3c61518ca16d6a47fe85484d4
dashevchenko <dshevchenko@thingsboard.io>
2022-11-14 10:59:28 +0200

    refactoring after PR review

--- msa/black-box-tests/src/test/java/org/thingsboard/server/msa/integration/MqttIntegrationTest.java
+++ msa/black-box-tests/src/test/java/org/thingsboard/server/msa/integration/MqttIntegrationTest.java
@@ -37,16 +37,6 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.awaitility.Awaitility;
-import org.bouncycastle.asn1.ASN1ObjectIdentifier;
-import org.bouncycastle.asn1.x500.X500Name;
-import org.bouncycastle.asn1.x509.BasicConstraints;
-import org.bouncycastle.cert.CertIOException;
-import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
-import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.operator.ContentSigner;
-import org.bouncycastle.operator.OperatorCreationException;
-import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
@@ -72,19 +62,6 @@ import org.thingsboard.server.common.data.rule.RuleNode;
 import org.thingsboard.server.msa.AbstractContainerTest;
 import org.thingsboard.server.msa.TestProperties;
 
-import java.io.IOException;
-import java.math.BigInteger;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.Provider;
-import java.security.SecureRandom;
-import java.security.Security;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateException;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -160,8 +137,6 @@ public class MqttIntegrationTest extends AbstractContainerTest {
     private Integration integration;
     private Converter uplinkConverter;
     private Converter downlinkConverter;
-    private final String[] arrivedMessage = new String[1];
-
 
     @BeforeMethod
     public void setUp() throws Exception {
@@ -436,45 +411,5 @@ public class MqttIntegrationTest extends AbstractContainerTest {
         private final String topic;
         private final String message;
     }
-
-    public static Certificate selfSign() throws OperatorCreationException, CertificateException, IOException, CertIOException, NoSuchAlgorithmException, NoSuchProviderException {
-        // generate a key pair
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA", "BC");
-        keyPairGenerator.initialize(4096, new SecureRandom());
-        KeyPair keyPair = keyPairGenerator.generateKeyPair();
-
-        Provider bcProvider = new BouncyCastleProvider();
-        Security.addProvider(bcProvider);
-
-        long now = System.currentTimeMillis();
-        Date startDate = new Date(now);
-
-        X500Name dnName = new X500Name("dc=name");
-        BigInteger certSerialNumber = new BigInteger(Long.toString(now)); // <-- Using the current timestamp as the certificate serial number
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(startDate);
-        calendar.add(Calendar.YEAR, 1); // <-- 1 Yr validity
-
-        Date endDate = calendar.getTime();
-
-        String signatureAlgorithm = "SHA256WithRSA"; // <-- Use appropriate signature algorithm based on your keyPair algorithm.
-
-        ContentSigner contentSigner = new JcaContentSignerBuilder(signatureAlgorithm).build(keyPair.getPrivate());
-
-        JcaX509v3CertificateBuilder certBuilder = new JcaX509v3CertificateBuilder(dnName, certSerialNumber, startDate, endDate, dnName, keyPair.getPublic());
-
-        // Extensions --------------------------
-
-        // Basic Constraints
-        BasicConstraints basicConstraints = new BasicConstraints(true); // <-- true for CA, false for EndEntity
-
-        certBuilder.addExtension(new ASN1ObjectIdentifier("2.5.29.19"), true, basicConstraints); // Basic Constraints is usually marked as critical.
-
-        // -------------------------------------
-
-        return new JcaX509CertificateConverter().setProvider(bcProvider).getCertificate(certBuilder.build(contentSigner));
-    }
-
 
 }
