eebb5e4a91e12e6e5f1734347ae4cf8874145e93
Tarnavskiy <starnavskiy@thingsboard.io>
2022-12-28 12:40:19 +0200

    Fixed all issues

--- ui-ngx/src/app/modules/home/components/home-components.module.ts
+++ ui-ngx/src/app/modules/home/components/home-components.module.ts
@@ -50,7 +50,6 @@ import { AddAttributeDialogComponent } from '@home/components/attribute/add-attr
 import { EditAttributeValuePanelComponent } from '@home/components/attribute/edit-attribute-value-panel.component';
 import { DashboardComponent } from '@home/components/dashboard/dashboard.component';
 import { WidgetComponent } from '@home/components/widget/widget.component';
-import { WidgetComponentService } from '@home/components/widget/widget-component.service';
 import { LegendComponent } from '@home/components/widget/legend.component';
 import { AliasesEntitySelectPanelComponent } from '@home/components/alias/aliases-entity-select-panel.component';
 import { AliasesEntitySelectComponent } from '@home/components/alias/aliases-entity-select.component';
@@ -551,7 +550,6 @@ import { IntegrationComponentModule } from '@home/components/integration/integra
     IntegrationWizardDialogComponent
   ],
   providers: [
-    WidgetComponentService,
     CustomDialogService,
     ImportExportService,
     GroupConfigTableConfigService,
