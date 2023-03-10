fc1717c68a3495cf7b9f1c861e0fce917b63a65f
Volodymyr Babak <volodymyr.babak@gmail.com>
2023-03-10 17:53:30 +0200

    Fix upgrade script for docker images

--- application/src/main/java/org/thingsboard/server/install/ThingsboardInstallService.java
+++ application/src/main/java/org/thingsboard/server/install/ThingsboardInstallService.java
@@ -232,7 +232,8 @@ public class ThingsboardInstallService {
                             log.info("Upgrading ThingsBoard from version 3.4.0 to 3.4.1 ...");
                             databaseEntitiesUpgradeService.upgradeDatabase("3.4.0");
                         case "3.4.1":
-                            log.info("Upgrading ThingsBoard from version 3.4.1 to 3.4.2 ...");
+                        case "3.4.2":
+                            log.info("Upgrading ThingsBoard from version 3.4.1 to 3.4.3 ...");
                             databaseEntitiesUpgradeService.upgradeDatabase("3.4.1");
                             dataUpdateService.updateData("3.4.1");
                         case "3.4.3":
