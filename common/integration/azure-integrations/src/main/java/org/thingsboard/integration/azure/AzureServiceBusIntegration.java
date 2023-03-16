a635aceac80e631e7e0e37232b3c87f17fcda95e
dashevchenko <dshevchenko@thingsboard.io>
2023-03-16 18:26:57 +0200

    added connection check

--- common/integration/azure-integrations/src/main/java/org/thingsboard/integration/azure/AzureServiceBusIntegration.java
+++ common/integration/azure-integrations/src/main/java/org/thingsboard/integration/azure/AzureServiceBusIntegration.java
@@ -31,9 +31,6 @@
 package org.thingsboard.integration.azure;
 
 import com.azure.messaging.servicebus.ServiceBusClientBuilder;
-import com.azure.messaging.servicebus.ServiceBusErrorContext;
-import com.azure.messaging.servicebus.ServiceBusException;
-import com.azure.messaging.servicebus.ServiceBusFailureReason;
 import com.azure.messaging.servicebus.ServiceBusMessage;
 import com.azure.messaging.servicebus.ServiceBusMessageBatch;
 import com.azure.messaging.servicebus.ServiceBusProcessorClient;
@@ -54,6 +51,8 @@ import org.thingsboard.integration.api.data.IntegrationMetaData;
 import org.thingsboard.integration.api.data.UplinkData;
 import org.thingsboard.integration.api.data.UplinkMetaData;
 import org.thingsboard.server.common.data.StringUtils;
+import org.thingsboard.server.common.data.exception.ThingsboardException;
+import org.thingsboard.server.common.data.integration.Integration;
 import org.thingsboard.server.common.msg.TbMsg;
 
 import java.io.IOException;
@@ -65,7 +64,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class AzureServiceBusIntegration extends AbstractIntegration<AzureServiceBusIntegrationMsg> {
@@ -129,6 +127,18 @@ public class AzureServiceBusIntegration extends AbstractIntegration<AzureService
         }
     }
 
+    @Override
+    public void checkConnection(Integration integration, IntegrationContext ctx) throws ThingsboardException {
+        var configuration = getClientConfiguration(
+                integration.getConfiguration().get("clientConfiguration"),
+                AzureServiceBusClientConfiguration.class
+        );
+
+        try (var consumerClient = buildConsumerClient(configuration)) {
+            log.debug("Service bus consumer connection checked");
+        }
+    }
+
     protected void processDownLinkMsg(IntegrationContext context, TbMsg msg) {
         String status = "OK";
         Exception exception = null;
@@ -167,40 +177,6 @@ public class AzureServiceBusIntegration extends AbstractIntegration<AzureService
         log.debug("Processing message. Session: [{}], Sequence #: [{}]. Contents: [{}]", message.getMessageId(),
                 message.getSequenceNumber(), message.getBody());
         process(new AzureServiceBusIntegrationMsg(context));
-    }
-
-    private void processError(ServiceBusErrorContext context, CountDownLatch countdownLatch) {
-        log.error("Error when receiving messages from namespace: [{}]. Entity: [{}]",
-                context.getFullyQualifiedNamespace(), context.getEntityPath());
-
-        if (!(context.getException() instanceof ServiceBusException)) {
-            log.error("Non-ServiceBusException occurred: [{}]", context.getException().getMessage());
-            return;
-        }
-
-        ServiceBusException exception = (ServiceBusException) context.getException();
-        ServiceBusFailureReason reason = exception.getReason();
-
-        if (reason == ServiceBusFailureReason.MESSAGING_ENTITY_DISABLED
-                || reason == ServiceBusFailureReason.MESSAGING_ENTITY_NOT_FOUND
-                || reason == ServiceBusFailureReason.UNAUTHORIZED) {
-            log.error("An unrecoverable error occurred. Stopping processing with reason [{}]: [{}]",
-                    reason, exception.getMessage());
-
-            countdownLatch.countDown();
-        } else if (reason == ServiceBusFailureReason.MESSAGE_LOCK_LOST) {
-            log.error("Message lock lost for message: [{}]", context.getException().getMessage());
-        } else if (reason == ServiceBusFailureReason.SERVICE_BUSY) {
-            try {
-                // Choosing an arbitrary amount of time to wait until trying again.
-                TimeUnit.SECONDS.sleep(1);
-            } catch (InterruptedException e) {
-                log.error("Unable to sleep for period of time");
-            }
-        } else {
-            log.error("Error source [{}], reason %s, message: [{}]", context.getErrorSource(),
-                    reason, context.getException());
-        }
     }
 
     private boolean doProcessDownLinkMsg(IntegrationContext context, TbMsg msg) throws Exception {
@@ -267,7 +243,7 @@ public class AzureServiceBusIntegration extends AbstractIntegration<AzureService
                 .topicName(configuration.getTopicName())
                 .subscriptionName(configuration.getSubName())
                 .processMessage(this::processMessage)
-                .processError(context -> processError(context, countdownLatch))
+                .processError(error -> log.error("It was trouble when receiving: " + error.getException().getMessage()))
                 .buildProcessorClient();
     }
 
