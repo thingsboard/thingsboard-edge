c9bea0f05f63db3ef534adb01f5b0e7492903acb
Seraphym-Tuhai <stuhai@thingsboard.io>
2023-03-28 12:52:27 +0300

    small refactoring

--- msa/black-box-tests/src/test/java/org/thingsboard/server/msa/ui/base/AbstractBasePage.java
+++ msa/black-box-tests/src/test/java/org/thingsboard/server/msa/ui/base/AbstractBasePage.java
@@ -51,7 +51,7 @@ import java.util.concurrent.TimeUnit;
 import static org.assertj.core.api.Assertions.fail;
 
 abstract public class AbstractBasePage {
-    public static final long WAIT_TIMEOUT = TimeUnit.SECONDS.toMillis(5);
+    public static final long WAIT_TIMEOUT = TimeUnit.SECONDS.toMillis(30);
     protected WebDriver driver;
     protected WebDriverWait wait;
     protected Actions actions;
