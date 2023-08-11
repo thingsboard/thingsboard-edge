07fe419efa94ffb25288656f99115b56f9aa0efe
Artem Dzhereleiko <Dgerik2015@gmail.com>
2023-08-11 16:23:14 +0300

    UI: Refactoring uplink data converter for new design style

--- ui-ngx/src/app/modules/home/components/converter/converter.component.ts
+++ ui-ngx/src/app/modules/home/components/converter/converter.component.ts
@@ -68,7 +68,7 @@ import { Observable, Subject } from 'rxjs';
 @Component({
   selector: 'tb-converter',
   templateUrl: './converter.component.html',
-  styleUrls: []
+  styleUrls: ['./converter.component.scss']
 })
 export class ConverterComponent extends EntityComponent<Converter> implements OnInit, OnDestroy {
 
