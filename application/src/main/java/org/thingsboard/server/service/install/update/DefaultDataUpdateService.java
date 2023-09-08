1b799d47c621ab2546e16f79fe034d091ef69f09
Sergey Matvienko <smatvienko@thingsboard.io>
2023-09-08 18:45:48 +0200

    fixed imports for PE

--- application/src/main/java/org/thingsboard/server/service/install/update/DefaultDataUpdateService.java
+++ application/src/main/java/org/thingsboard/server/service/install/update/DefaultDataUpdateService.java
@@ -142,6 +142,7 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
