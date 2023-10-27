3a133feaf261ee4bb577ac86e240062412c0dc8c
Andrii Landiak <andriylandiak@gmail.com>
2023-10-27 14:53:02 +0300

    Fix containerTestCSuite for edge bbtests

--- msa/edge-black-box-tests/src/test/java/org/thingsboard/server/msa/ContainerTestSuite.java
+++ msa/edge-black-box-tests/src/test/java/org/thingsboard/server/msa/ContainerTestSuite.java
@@ -34,7 +34,7 @@ import static org.thingsboard.server.msa.AbstractContainerTest.TB_EDGE_SERVICE_N
 import static org.thingsboard.server.msa.AbstractContainerTest.TB_MONOLITH_SERVICE_NAME;
 
 @RunWith(ClasspathSuite.class)
-@ClasspathSuite.ClassnameFilters({"org.thingsboard.server.msa.edge.ResourceClientTest"})
+@ClasspathSuite.ClassnameFilters({"org.thingsboard.server.msa.edge.*Test"})
 @Slf4j
 public class ContainerTestSuite {
 
