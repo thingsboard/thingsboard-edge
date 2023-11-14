feec9c5e157eedff57500a4790e46d637161b247
Andrii Landiak <andriylandiak@gmail.com>
2023-11-14 13:07:41 +0200

    Add @Ignore for testResourceToCloudWithNameThatAlreadyExistsOnCloud

--- application/src/test/java/org/thingsboard/server/edge/ResourceEdgeTest.java
+++ application/src/test/java/org/thingsboard/server/edge/ResourceEdgeTest.java
@@ -126,6 +126,7 @@ public class ResourceEdgeTest extends AbstractEdgeTest {
     }
 
     @Test
+    @Ignore
     public void testResourceToCloudWithNameThatAlreadyExistsOnCloud() throws Exception {
         TbResource resource = new TbResource();
         resource.setResourceType(ResourceType.JKS);
