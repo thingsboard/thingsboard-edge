4204ae0bfadd2f3f07ac81d731becfb70fd2a70a
Andrii Landiak <andriylandiak@gmail.com>
2024-03-12 18:00:57 +0200

    Add DeviceGroup fetcher

---
+++ application/src/main/java/org/thingsboard/server/service/edge/rpc/fetch/DeviceGroupOtaPackageEdgeEventFetcher.java
@@ -0,0 +1,88 @@
+/**
+ * ThingsBoard, Inc. ("COMPANY") CONFIDENTIAL
+ *
+ * Copyright © 2016-2024 ThingsBoard, Inc. All Rights Reserved.
+ *
+ * NOTICE: All information contained herein is, and remains
+ * the property of ThingsBoard, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to ThingsBoard, Inc.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ *
+ * Dissemination of this information or reproduction of this material is strictly forbidden
+ * unless prior written permission is obtained from COMPANY.
+ *
+ * Access to the source code contained herein is hereby forbidden to anyone except current COMPANY employees,
+ * managers or contractors who have executed Confidentiality and Non-disclosure agreements
+ * explicitly covering such access.
+ *
+ * The copyright notice above does not evidence any actual or intended publication
+ * or disclosure  of  this source code, which includes
+ * information that is confidential and/or proprietary, and is a trade secret, of  COMPANY.
+ * ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC  PERFORMANCE,
+ * OR PUBLIC DISPLAY OF OR THROUGH USE  OF THIS  SOURCE CODE  WITHOUT
+ * THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED,
+ * AND IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES.
+ * THE RECEIPT OR POSSESSION OF THIS SOURCE CODE AND/OR RELATED INFORMATION
+ * DOES NOT CONVEY OR IMPLY ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS,
+ * OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
+ */
+package org.thingsboard.server.service.edge.rpc.fetch;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.thingsboard.common.util.JacksonUtil;
+import org.thingsboard.server.common.data.EdgeUtils;
+import org.thingsboard.server.common.data.EntityType;
+import org.thingsboard.server.common.data.edge.Edge;
+import org.thingsboard.server.common.data.edge.EdgeEvent;
+import org.thingsboard.server.common.data.edge.EdgeEventActionType;
+import org.thingsboard.server.common.data.edge.EdgeEventType;
+import org.thingsboard.server.common.data.group.EntityGroup;
+import org.thingsboard.server.common.data.id.TenantId;
+import org.thingsboard.server.common.data.ota.DeviceGroupOtaPackage;
+import org.thingsboard.server.common.data.ota.OtaPackageType;
+import org.thingsboard.server.common.data.page.PageData;
+import org.thingsboard.server.common.data.page.PageLink;
+import org.thingsboard.server.dao.group.EntityGroupService;
+import org.thingsboard.server.dao.ota.DeviceGroupOtaPackageService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@Slf4j
+public class DeviceGroupOtaPackageEdgeEventFetcher implements EdgeEventFetcher {
+
+    private final DeviceGroupOtaPackageService deviceGroupOtaPackageService;
+
+    private final EntityGroupService entityGroupService;
+
+    @Override
+    public PageLink getPageLink(int pageSize) {
+        return null;
+    }
+
+    @Override
+    public PageData<EdgeEvent> fetchEdgeEvents(TenantId tenantId, Edge edge, PageLink pageLink) throws Exception {
+        PageData<EntityGroup> pageData = entityGroupService.findEdgeEntityGroupsByType(tenantId, edge.getId(), EntityType.DEVICE, pageLink);
+        List<EdgeEvent> result = new ArrayList<>();
+        if (!pageData.getData().isEmpty()) {
+            for (EntityGroup entityGroup : pageData.getData()) {
+                DeviceGroupOtaPackage firmware = deviceGroupOtaPackageService.findDeviceGroupOtaPackageByGroupIdAndType(entityGroup.getId(), OtaPackageType.FIRMWARE);
+                if (firmware != null) {
+                    result.add(EdgeUtils.constructEdgeEvent(tenantId, edge.getId(), EdgeEventType.DEVICE_GROUP_OTA,
+                            EdgeEventActionType.UPDATED, firmware.getOtaPackageId(), JacksonUtil.valueToTree(firmware)));
+                }
+                DeviceGroupOtaPackage software = deviceGroupOtaPackageService.findDeviceGroupOtaPackageByGroupIdAndType(entityGroup.getId(), OtaPackageType.SOFTWARE);
+                if (software != null) {
+                    result.add(EdgeUtils.constructEdgeEvent(tenantId, edge.getId(), EdgeEventType.DEVICE_GROUP_OTA,
+                            EdgeEventActionType.UPDATED, software.getOtaPackageId(), JacksonUtil.valueToTree(software)));
+                }
+            }
+        }
+        return new PageData<>(result, 1, result.size(), false);
+    }
+
+}
