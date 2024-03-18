691d51d767acfc799c32f20a6070e5b44da2b131
YevhenBondarenko <ybondarenko@thingsboard.io>
2024-03-18 17:29:32 +0100

    added swagger docs for reportController

--- application/src/main/java/org/thingsboard/server/controller/ReportController.java
+++ application/src/main/java/org/thingsboard/server/controller/ReportController.java
@@ -31,6 +31,8 @@
 package org.thingsboard.server.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
@@ -51,6 +53,7 @@ import org.thingsboard.server.common.data.exception.ThingsboardException;
 import org.thingsboard.server.common.data.id.DashboardId;
 import org.thingsboard.server.common.data.permission.Operation;
 import org.thingsboard.server.common.data.report.ReportConfig;
+import org.thingsboard.server.common.data.report.ReportData;
 import org.thingsboard.server.queue.util.TbCoreComponent;
 import org.thingsboard.server.service.security.model.SecurityUser;
 import org.thingsboard.server.service.security.model.UserPrincipal;
@@ -59,6 +62,13 @@ import org.thingsboard.server.utils.MiscUtils;
 import javax.servlet.http.HttpServletRequest;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.function.Consumer;
+
+import static org.thingsboard.server.controller.ControllerConstants.DASHBOARD_ID_PARAM_DESCRIPTION;
+import static org.thingsboard.server.controller.ControllerConstants.MARKDOWN_CODE_BLOCK_END;
+import static org.thingsboard.server.controller.ControllerConstants.MARKDOWN_CODE_BLOCK_START;
+import static org.thingsboard.server.controller.ControllerConstants.REPORT_PARAMS_EXAMPLE;
+import static org.thingsboard.server.controller.ControllerConstants.TENANT_OR_CUSTOMER_AUTHORITY_PARAGRAPH;
 
 @RestController
 @TbCoreComponent
@@ -72,13 +82,19 @@ public class ReportController extends BaseController {
 
     public static final String DASHBOARD_ID = "dashboardId";
 
+    @ApiOperation(value = "Download dashboard report (downloadDashboardReport)",
+            notes = "Generate and download a report from the specified dashboard. " +
+                    "The request payload is a JSON object with params of report. For example:\n\n"
+                    + MARKDOWN_CODE_BLOCK_START + REPORT_PARAMS_EXAMPLE + MARKDOWN_CODE_BLOCK_END + "\n" + TENANT_OR_CUSTOMER_AUTHORITY_PARAGRAPH)
     @PreAuthorize("hasAnyAuthority('TENANT_ADMIN', 'CUSTOMER_USER')")
-    @RequestMapping(value = "/report/{dashboardId}/download", method = RequestMethod.POST)
+    @RequestMapping(value = "/report/{dashboardId}/download", method = RequestMethod.POST, produces = {"application/pdf", "image/jpeg", "image/png"})
     @ResponseBody
-    public DeferredResult<ResponseEntity> downloadDashboardReport(@PathVariable(DASHBOARD_ID) String strDashboardId,
-                                                                  @RequestBody JsonNode reportParams,
-                                                                  HttpServletRequest request) throws ThingsboardException {
-        DeferredResult<ResponseEntity> result = new DeferredResult<>();
+    public DeferredResult<ResponseEntity<Resource>> downloadDashboardReport(@ApiParam(value = DASHBOARD_ID_PARAM_DESCRIPTION, required = true)
+                                                                            @PathVariable(DASHBOARD_ID) String strDashboardId,
+                                                                            @ApiParam(example = REPORT_PARAMS_EXAMPLE, required = true)
+                                                                            @RequestBody JsonNode reportParams,
+                                                                            HttpServletRequest request) throws ThingsboardException {
+        DeferredResult<ResponseEntity<Resource>> result = new DeferredResult<>();
         checkParameter(DASHBOARD_ID, strDashboardId);
         try {
             DashboardId dashboardId = new DashboardId(toUUID(strDashboardId));
@@ -95,31 +111,23 @@ public class ReportController extends BaseController {
             }
             reportService.
                     generateDashboardReport(baseUrl, dashboardId, getTenantId(), currentUser.getId(), publicId, name, reportParams,
-                            reportData -> {
-                                ByteArrayResource resource = new ByteArrayResource(reportData.getData());
-                                ResponseEntity<Resource> response = ResponseEntity.ok().
-                                        header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + reportData.getName())
-                                        .header("x-filename", reportData.getName())
-                                        .contentLength(resource.contentLength())
-                                        .contentType(parseMediaType(reportData.getContentType()))
-                                        .body(resource);
-                                result.setResult(response);
-                            },
-                            throwable -> {
-                                result.setErrorResult(throwable);
-                            });
+                            onSuccess(result),
+                            result::setErrorResult);
         } catch (Exception e) {
             result.setErrorResult(e);
         }
         return result;
     }
 
+    @ApiOperation(value = "Download test report (downloadTestReport)",
+            notes = "Generate and download test report." + TENANT_OR_CUSTOMER_AUTHORITY_PARAGRAPH)
     @PreAuthorize("hasAnyAuthority('TENANT_ADMIN', 'CUSTOMER_USER')")
-    @RequestMapping(value = "/report/test", method = RequestMethod.POST)
+    @RequestMapping(value = "/report/test", method = RequestMethod.POST, produces = {"application/pdf", "image/jpeg", "image/png"})
     @ResponseBody
-    public DeferredResult<ResponseEntity> downloadTestReport(@RequestBody ReportConfig reportConfig,
-                                                             @RequestParam(required = false) String reportsServerEndpointUrl) {
-        DeferredResult<ResponseEntity> result = new DeferredResult<>();
+    public DeferredResult<ResponseEntity<Resource>> downloadTestReport(@RequestBody ReportConfig reportConfig,
+                                                                       @ApiParam(value = "A string value representing the report server endpoint.", example = "http://localhost:8383")
+                                                                       @RequestParam(required = false) String reportsServerEndpointUrl) {
+        DeferredResult<ResponseEntity<Resource>> result = new DeferredResult<>();
         try {
             String strDashboardId = reportConfig.getDashboardId();
             checkParameter(DASHBOARD_ID, strDashboardId);
@@ -127,28 +135,24 @@ public class ReportController extends BaseController {
             DashboardId dashboardId = new DashboardId(toUUID(strDashboardId));
             checkDashboardInfoId(dashboardId, Operation.READ);
 
-            reportService.
-                    generateReport(
-                            getTenantId(),
-                            reportConfig,
-                            reportsServerEndpointUrl,
-                            reportData -> {
-                                ByteArrayResource resource = new ByteArrayResource(reportData.getData());
-                                ResponseEntity<Resource> response = ResponseEntity.ok().
-                                        header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + reportData.getName())
-                                        .header("x-filename", reportData.getName())
-                                        .contentLength(resource.contentLength())
-                                        .contentType(parseMediaType(reportData.getContentType()))
-                                        .body(resource);
-                                result.setResult(response);
-                            },
-                            throwable -> {
-                                result.setErrorResult(throwable);
-                            });
+            reportService.generateReport(getTenantId(), reportConfig, reportsServerEndpointUrl, onSuccess(result), result::setErrorResult);
         } catch (Exception e) {
             result.setErrorResult(e);
         }
         return result;
+    }
+
+    private Consumer<ReportData> onSuccess(DeferredResult<ResponseEntity<Resource>> result) {
+        return reportData -> {
+            ByteArrayResource resource = new ByteArrayResource(reportData.getData());
+            ResponseEntity<Resource> response = ResponseEntity.ok().
+                    header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + reportData.getName())
+                    .header("x-filename", reportData.getName())
+                    .contentLength(resource.contentLength())
+                    .contentType(parseMediaType(reportData.getContentType()))
+                    .body(resource);
+            result.setResult(response);
+        };
     }
 
 }
