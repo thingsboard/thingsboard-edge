28acaf7ac0ed660f676ca6fcc0d0879669d8c32e
IrynaMatveieva <irina.m2004@icloud.com>
2024-04-08 12:16:04 +0300

    added tests for add to group and remove from group nodes

---
+++ rule-engine/rule-engine-components/src/test/java/org/thingsboard/rule/engine/action/TbAddToGroupNodeTest.java
@@ -0,0 +1,196 @@
+/**
+ * ThingsBoard, Inc. ("COMPANY") CONFIDENTIAL
+ *
+ * Copyright © 2016-2024 ThingsBoard, Inc. All Rights Reserved.
+ *
+ * NOTICE: All information contained herein is, and remains
+ * the property of ThingsBoard, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to ThingsBoard, Inc.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ *
+ * Dissemination of this information or reproduction of this material is strictly forbidden
+ * unless prior written permission is obtained from COMPANY.
+ *
+ * Access to the source code contained herein is hereby forbidden to anyone except current COMPANY employees,
+ * managers or contractors who have executed Confidentiality and Non-disclosure agreements
+ * explicitly covering such access.
+ *
+ * The copyright notice above does not evidence any actual or intended publication
+ * or disclosure  of  this source code, which includes
+ * information that is confidential and/or proprietary, and is a trade secret, of  COMPANY.
+ * ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC  PERFORMANCE,
+ * OR PUBLIC DISPLAY OF OR THROUGH USE  OF THIS  SOURCE CODE  WITHOUT
+ * THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED,
+ * AND IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES.
+ * THE RECEIPT OR POSSESSION OF THIS SOURCE CODE AND/OR RELATED INFORMATION
+ * DOES NOT CONVEY OR IMPLY ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS,
+ * OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
+ */
+package org.thingsboard.rule.engine.action;
+
+import com.google.common.util.concurrent.Futures;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.thingsboard.common.util.JacksonUtil;
+import org.thingsboard.common.util.ListeningExecutor;
+import org.thingsboard.rule.engine.TestDbCallbackExecutor;
+import org.thingsboard.rule.engine.api.TbContext;
+import org.thingsboard.rule.engine.api.TbNodeConfiguration;
+import org.thingsboard.rule.engine.api.TbNodeException;
+import org.thingsboard.rule.engine.api.TbPeContext;
+import org.thingsboard.server.common.data.Device;
+import org.thingsboard.server.common.data.EntityType;
+import org.thingsboard.server.common.data.group.EntityGroup;
+import org.thingsboard.server.common.data.id.DeviceId;
+import org.thingsboard.server.common.data.id.EntityGroupId;
+import org.thingsboard.server.common.data.id.TenantId;
+import org.thingsboard.server.common.data.msg.TbMsgType;
+import org.thingsboard.server.common.msg.TbMsg;
+import org.thingsboard.server.common.msg.TbMsgMetaData;
+import org.thingsboard.server.dao.device.DeviceService;
+import org.thingsboard.server.dao.group.EntityGroupService;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TbAddToGroupNodeTest {
+
+    private final TenantId TENANT_ID = new TenantId(UUID.fromString("b744dd76-79ea-442c-a7b8-c06c7f568488"));
+    private final DeviceId DEVICE_ID = new DeviceId(UUID.fromString("48641148-1f52-4d9e-ad74-238515365df5"));
+    private final ListeningExecutor dbCallbackExecutor = new TestDbCallbackExecutor();
+
+    private TbAddToGroupNode node;
+    private TbAddToGroupConfiguration config;
+
+    @Mock
+    private TbContext ctxMock;
+    @Mock
+    private TbPeContext peContextMock;
+    @Mock
+    private EntityGroupService entityGroupServiceMock;
+    @Mock
+    private DeviceService deviceServiceMock;
+
+    @BeforeEach
+    public void setUp() {
+        node = new TbAddToGroupNode();
+        config = new TbAddToGroupConfiguration().defaultConfiguration();
+    }
+
+    @Test
+    public void givenEntityWithoutEntityGroup_whenOnMsg_thenAddToExistingGroup() throws TbNodeException {
+        EntityGroupId entityGroupId = new EntityGroupId(UUID.fromString("0aba647a-c081-42da-83eb-2fe9dc920bcb"));
+        config.setGroupNamePattern("${groupName}");
+        var configuration = new TbNodeConfiguration(JacksonUtil.valueToTree(config));
+        node.init(ctxMock, configuration);
+
+        when(ctxMock.getPeContext()).thenReturn(peContextMock);
+        when(ctxMock.getTenantId()).thenReturn(TENANT_ID);
+        when(peContextMock.getOwner(any(), any())).thenReturn(TENANT_ID);
+        when(ctxMock.getDbCallbackExecutor()).thenReturn(dbCallbackExecutor);
+        when(peContextMock.getEntityGroupService()).thenReturn(entityGroupServiceMock);
+        when(entityGroupServiceMock.findEntityGroupByTypeAndNameAsync(any(), any(), any(), any()))
+                .thenReturn(Futures.immediateFuture(Optional.of(new EntityGroup(entityGroupId))));
+
+        TbMsg msg = TbMsg.newMsg(
+                TbMsgType.POST_TELEMETRY_REQUEST, DEVICE_ID,
+                new TbMsgMetaData(Map.of("groupName", "Device Group")), TbMsg.EMPTY_JSON_OBJECT);
+        node.onMsg(ctxMock, msg);
+
+        verify(peContextMock).getOwner(eq(TENANT_ID), eq(DEVICE_ID));
+        verify(entityGroupServiceMock).findEntityGroupByTypeAndNameAsync(eq(TENANT_ID), eq(TENANT_ID), eq(EntityType.DEVICE), eq("Device Group"));
+        verify(entityGroupServiceMock).addEntityToEntityGroup(eq(TENANT_ID), eq(entityGroupId), eq(DEVICE_ID));
+        verify(ctxMock).tellNext(msg, "Success");
+    }
+
+    @Test
+    public void givenEntityAndEntityGroupNotExistAndCreateGroupIfNotExistsIsTrue_whenOnMsg_thenCreateAndAddEntityToNewGroup() throws TbNodeException {
+        EntityGroupId entityGroupId = new EntityGroupId(UUID.fromString("968ac449-ef4c-4ad9-9574-a955d036b5d5"));
+        config.setGroupNamePattern("${groupName}");
+        config.setCreateGroupIfNotExists(true);
+        var configuration = new TbNodeConfiguration(JacksonUtil.valueToTree(config));
+        node.init(ctxMock, configuration);
+
+        when(ctxMock.getPeContext()).thenReturn(peContextMock);
+        when(ctxMock.getTenantId()).thenReturn(TENANT_ID);
+        when(peContextMock.getOwner(any(), any())).thenReturn(TENANT_ID);
+        when(ctxMock.getDbCallbackExecutor()).thenReturn(dbCallbackExecutor);
+        when(peContextMock.getEntityGroupService()).thenReturn(entityGroupServiceMock);
+        when(entityGroupServiceMock.findEntityGroupByTypeAndNameAsync(any(), any(), any(), any()))
+                .thenReturn(Futures.immediateFuture(Optional.empty()));
+        when(entityGroupServiceMock.saveEntityGroup(any(), any(), any())).thenReturn(new EntityGroup(entityGroupId));
+
+        TbMsg msg = TbMsg.newMsg(
+                TbMsgType.POST_TELEMETRY_REQUEST, DEVICE_ID,
+                new TbMsgMetaData(Map.of("groupName", "Another Device Group")), TbMsg.EMPTY_JSON_OBJECT);
+        node.onMsg(ctxMock, msg);
+
+        verify(peContextMock).getOwner(eq(TENANT_ID), eq(DEVICE_ID));
+        verify(entityGroupServiceMock).findEntityGroupByTypeAndNameAsync(eq(TENANT_ID), eq(TENANT_ID), eq(EntityType.DEVICE), eq("Another Device Group"));
+        EntityGroup newEntityGroup = new EntityGroup();
+        newEntityGroup.setName("Another Device Group");
+        newEntityGroup.setType(EntityType.DEVICE);
+        verify(entityGroupServiceMock).saveEntityGroup(eq(TENANT_ID), eq(TENANT_ID), eq(newEntityGroup));
+        verify(entityGroupServiceMock).addEntityToEntityGroup(eq(TENANT_ID), eq(entityGroupId), eq(DEVICE_ID));
+        verify(ctxMock).tellNext(msg, "Success");
+    }
+
+    @Test
+    void givenEntityWithEntityGroupAndRemoveFromCurrentGroups_whenOnMsg_thenAddToExistingGroupAndRemoveFromCurrentGroups() throws TbNodeException {
+        EntityGroupId currentEntityGroupId = new EntityGroupId(UUID.fromString("95017c30-e91a-4387-8fbf-4913e881ab05"));
+        EntityGroupId newEntityGroupId = new EntityGroupId(UUID.fromString("0aba647a-c081-42da-83eb-2fe9dc920bcb"));
+        Device device = new Device(DEVICE_ID);
+        device.setTenantId(TENANT_ID);
+        config.setGroupNamePattern("${groupName}");
+        config.setRemoveFromCurrentGroups(true);
+        var configuration = new TbNodeConfiguration(JacksonUtil.valueToTree(config));
+        node.init(ctxMock, configuration);
+
+        when(ctxMock.getPeContext()).thenReturn(peContextMock);
+        when(ctxMock.getTenantId()).thenReturn(TENANT_ID);
+        when(peContextMock.getOwner(any(), any())).thenReturn(TENANT_ID);
+        when(ctxMock.getDbCallbackExecutor()).thenReturn(dbCallbackExecutor);
+        when(peContextMock.getEntityGroupService()).thenReturn(entityGroupServiceMock);
+        when(entityGroupServiceMock.findEntityGroupByTypeAndNameAsync(any(), any(), any(), any()))
+                .thenReturn(Futures.immediateFuture(Optional.of(new EntityGroup(newEntityGroupId))));
+        when(entityGroupServiceMock.findEntityGroupsForEntityAsync(any(), any()))
+                .thenReturn(Futures.immediateFuture(List.of(currentEntityGroupId)));
+        when(ctxMock.getDeviceService()).thenReturn(deviceServiceMock);
+        when(deviceServiceMock.findDeviceById(any(), any())).thenReturn(device);
+        when(entityGroupServiceMock.findEntityGroupByTypeAndNameAsync(any(), any(), any(), eq(EntityGroup.GROUP_ALL_NAME)))
+                .thenReturn(Futures.immediateFuture(Optional.of(new EntityGroup(currentEntityGroupId))));
+
+        TbMsg msg = TbMsg.newMsg(
+                TbMsgType.POST_TELEMETRY_REQUEST, DEVICE_ID,
+                new TbMsgMetaData(Map.of("groupName", "Device Group")), TbMsg.EMPTY_JSON_OBJECT);
+        node.onMsg(ctxMock, msg);
+
+        verify(peContextMock).getOwner(eq(TENANT_ID), eq(DEVICE_ID));
+        verify(entityGroupServiceMock).findEntityGroupByTypeAndNameAsync(eq(TENANT_ID), eq(TENANT_ID), eq(EntityType.DEVICE), eq("Device Group"));
+        verify(entityGroupServiceMock).findEntityGroupsForEntityAsync(eq(TENANT_ID), eq(DEVICE_ID));
+        verify(deviceServiceMock).findDeviceById(eq(TENANT_ID), eq(DEVICE_ID));
+        verify(entityGroupServiceMock).findEntityGroupByTypeAndNameAsync(eq(TENANT_ID), eq(TENANT_ID), eq(EntityType.DEVICE), eq(EntityGroup.GROUP_ALL_NAME));
+        verify(entityGroupServiceMock).addEntityToEntityGroup(eq(TENANT_ID), eq(newEntityGroupId), eq(DEVICE_ID));
+        verify(ctxMock).tellNext(msg, "Success");
+    }
+
+    @Test
+    public void givenDefaultConfig_whenInit_thenOk() {
+        var configuration = new TbNodeConfiguration(JacksonUtil.valueToTree(config));
+        assertThatNoException().isThrownBy(() -> node.init(ctxMock, configuration));
+    }
+}
---
+++ rule-engine/rule-engine-components/src/test/java/org/thingsboard/rule/engine/action/TbRemoveFromGroupNodeTest.java
@@ -0,0 +1,144 @@
+/**
+ * ThingsBoard, Inc. ("COMPANY") CONFIDENTIAL
+ *
+ * Copyright © 2016-2024 ThingsBoard, Inc. All Rights Reserved.
+ *
+ * NOTICE: All information contained herein is, and remains
+ * the property of ThingsBoard, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to ThingsBoard, Inc.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ *
+ * Dissemination of this information or reproduction of this material is strictly forbidden
+ * unless prior written permission is obtained from COMPANY.
+ *
+ * Access to the source code contained herein is hereby forbidden to anyone except current COMPANY employees,
+ * managers or contractors who have executed Confidentiality and Non-disclosure agreements
+ * explicitly covering such access.
+ *
+ * The copyright notice above does not evidence any actual or intended publication
+ * or disclosure  of  this source code, which includes
+ * information that is confidential and/or proprietary, and is a trade secret, of  COMPANY.
+ * ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC  PERFORMANCE,
+ * OR PUBLIC DISPLAY OF OR THROUGH USE  OF THIS  SOURCE CODE  WITHOUT
+ * THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED,
+ * AND IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES.
+ * THE RECEIPT OR POSSESSION OF THIS SOURCE CODE AND/OR RELATED INFORMATION
+ * DOES NOT CONVEY OR IMPLY ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS,
+ * OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
+ */
+package org.thingsboard.rule.engine.action;
+
+import com.google.common.util.concurrent.Futures;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.thingsboard.common.util.JacksonUtil;
+import org.thingsboard.common.util.ListeningExecutor;
+import org.thingsboard.rule.engine.TestDbCallbackExecutor;
+import org.thingsboard.rule.engine.api.TbContext;
+import org.thingsboard.rule.engine.api.TbNodeConfiguration;
+import org.thingsboard.rule.engine.api.TbNodeException;
+import org.thingsboard.rule.engine.api.TbPeContext;
+import org.thingsboard.server.common.data.EntityType;
+import org.thingsboard.server.common.data.group.EntityGroup;
+import org.thingsboard.server.common.data.id.DeviceId;
+import org.thingsboard.server.common.data.id.EntityGroupId;
+import org.thingsboard.server.common.data.id.TenantId;
+import org.thingsboard.server.common.data.msg.TbMsgType;
+import org.thingsboard.server.common.msg.TbMsg;
+import org.thingsboard.server.common.msg.TbMsgMetaData;
+import org.thingsboard.server.dao.group.EntityGroupService;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TbRemoveFromGroupNodeTest {
+
+    private final TenantId TENANT_ID = new TenantId(UUID.fromString("cb27b618-e85b-4a65-b270-edc4b59fc01f"));
+    private final DeviceId DEVICE_ID = new DeviceId(UUID.fromString("961167a0-c6a7-44a9-ac2b-f1f40102ba97"));
+    private final ListeningExecutor dbCallbackExecutor = new TestDbCallbackExecutor();
+
+    private TbRemoveFromGroupNode node;
+    private TbRemoveFromGroupConfiguration config;
+
+    @Mock
+    private TbContext ctxMock;
+    @Mock
+    private TbPeContext peContextMock;
+    @Mock
+    private EntityGroupService entityGroupServiceMock;
+
+    @BeforeEach
+    public void setUp() {
+        node = new TbRemoveFromGroupNode();
+        config = new TbRemoveFromGroupConfiguration().defaultConfiguration();
+    }
+
+    @Test
+    public void givenDeviceWithDeviceGroup_whenOnMsg_thenRemoveFromGroup() throws TbNodeException {
+        EntityGroupId entityGroupId = new EntityGroupId(UUID.fromString("bf14ba3f-cf37-4b26-b292-79aba01effd7"));
+        config.setGroupNamePattern("${groupName}");
+        var configuration = new TbNodeConfiguration(JacksonUtil.valueToTree(config));
+        node.init(ctxMock, configuration);
+
+        when(ctxMock.getPeContext()).thenReturn(peContextMock);
+        when(peContextMock.getOwner(any(), any())).thenReturn(TENANT_ID);
+        when(ctxMock.getDbCallbackExecutor()).thenReturn(dbCallbackExecutor);
+        when(peContextMock.getEntityGroupService()).thenReturn(entityGroupServiceMock);
+        when(ctxMock.getTenantId()).thenReturn(TENANT_ID);
+        when(entityGroupServiceMock.findEntityGroupByTypeAndNameAsync(any(), any(), any(), any()))
+                .thenReturn(Futures.immediateFuture(Optional.of(new EntityGroup(entityGroupId))));
+
+        TbMsg msg = TbMsg.newMsg(
+                TbMsgType.POST_TELEMETRY_REQUEST, DEVICE_ID,
+                new TbMsgMetaData(Map.of("groupName", "Device Group")), TbMsg.EMPTY_JSON_OBJECT);
+        node.onMsg(ctxMock, msg);
+
+        verify(peContextMock).getOwner(eq(TENANT_ID), eq(DEVICE_ID));
+        verify(entityGroupServiceMock).findEntityGroupByTypeAndNameAsync(eq(TENANT_ID), eq(TENANT_ID), eq(EntityType.DEVICE), eq("Device Group"));
+        verify(entityGroupServiceMock).removeEntityFromEntityGroup(eq(TENANT_ID), eq(entityGroupId), eq(DEVICE_ID));
+        verify(ctxMock).tellNext(msg, "Success");
+    }
+
+    @Test
+    public void givenDeviceWithoutDeviceGroup_whenOnMsg_thenThrowsException() throws TbNodeException {
+        config.setGroupNamePattern("${groupName}");
+        var configuration = new TbNodeConfiguration(JacksonUtil.valueToTree(config));
+        node.init(ctxMock, configuration);
+
+        when(ctxMock.getPeContext()).thenReturn(peContextMock);
+        when(peContextMock.getOwner(any(), any())).thenReturn(TENANT_ID);
+        when(ctxMock.getTenantId()).thenReturn(TENANT_ID);
+        when(ctxMock.getDbCallbackExecutor()).thenReturn(dbCallbackExecutor);
+        when(peContextMock.getEntityGroupService()).thenReturn(entityGroupServiceMock);
+        when(entityGroupServiceMock.findEntityGroupByTypeAndNameAsync(any(), any(), any(), any()))
+                .thenReturn(Futures.immediateFuture(Optional.empty()));
+
+        TbMsg msg = TbMsg.newMsg(
+                TbMsgType.POST_TELEMETRY_REQUEST, DEVICE_ID,
+                new TbMsgMetaData(Map.of("groupName", "Device Group")), TbMsg.EMPTY_JSON_OBJECT);
+
+        assertThatThrownBy(() -> node.onMsg(ctxMock, msg))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("java.lang.RuntimeException: No entity group found with type '" + EntityType.DEVICE + " ' and name 'Device Group'.");
+    }
+
+    @Test
+    public void givenDefaultConfig_whenInit_thenOk() {
+        var configuration = new TbNodeConfiguration(JacksonUtil.valueToTree(config));
+        assertThatNoException().isThrownBy(() -> node.init(ctxMock, configuration));
+    }
+}
