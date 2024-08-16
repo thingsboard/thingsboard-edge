d618aee280c4932dddecd98dfc1a925957fb352b
Andrii Landiak <andriylandiak@gmail.com>
2024-08-16 16:57:02 +0300

    Add indexes for ts_kv_cloud_events

--- application/src/main/data/upgrade/3.7.0/schema_update.sql
+++ application/src/main/data/upgrade/3.7.0/schema_update.sql
@@ -57,10 +57,10 @@ ALTER TABLE tenant ADD COLUMN IF NOT EXISTS version BIGINT DEFAULT 1;
 -- edge-only: create new ts_kv_cloud_event table schema
 
 CREATE TABLE IF NOT EXISTS ts_kv_cloud_event (
-                                                 seq_id INT GENERATED ALWAYS AS IDENTITY,
-                                                 id uuid NOT NULL,
-                                                 created_time bigint NOT NULL,
-                                                 cloud_event_type varchar(255),
+    seq_id INT GENERATED ALWAYS AS IDENTITY,
+    id uuid NOT NULL,
+    created_time bigint NOT NULL,
+    cloud_event_type varchar(255),
     entity_id uuid,
     cloud_event_action varchar(255),
     entity_body varchar(10000000),
@@ -69,5 +69,10 @@ CREATE TABLE IF NOT EXISTS ts_kv_cloud_event (
     ) PARTITION BY RANGE(created_time);
 
 ALTER TABLE IF EXISTS ts_kv_cloud_event ALTER COLUMN seq_id SET CYCLE;
+
+CREATE INDEX IF NOT EXISTS idx_ts_kv_cloud_event_tenant_id_and_created_time ON ts_kv_cloud_event(tenant_id, created_time DESC);
+
+CREATE INDEX IF NOT EXISTS idx_ts_kv_cloud_event_tenant_id_entity_id_event_type_event_action_crt ON ts_kv_cloud_event
+    (tenant_id, entity_id, cloud_event_type, cloud_event_action, created_time DESC);
 
 -- edge-only: create new ts_kv_cloud_event table schema end
