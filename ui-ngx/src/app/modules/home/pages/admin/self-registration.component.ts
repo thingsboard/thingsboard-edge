993a0c7185a92b08ef67d98bfab4ac2d577e3dd4
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2024-11-20 16:53:07 +0200

    UI: Revert disable delete in self-registaration

--- ui-ngx/src/app/modules/home/pages/admin/self-registration.component.ts
+++ ui-ngx/src/app/modules/home/pages/admin/self-registration.component.ts
@@ -55,6 +55,8 @@ export class SelfRegistrationComponent extends PageComponent implements OnInit, 
 
   entityTypes = EntityType;
 
+  deleteDisabled = true;
+
   tinyMceOptions: Record<string, any> = {
     base_url: '/assets/tinymce',
     suffix: '.min',
@@ -167,8 +169,10 @@ export class SelfRegistrationComponent extends PageComponent implements OnInit, 
     } as WebSelfRegistrationParams;
     if (this.selfRegistrationParams.domainName && this.selfRegistrationParams.domainName.length) {
       this.registerLink = this.selfRegistrationService.getRegistrationLink(this.selfRegistrationParams.domainName);
+      this.deleteDisabled = false;
     } else {
       this.registerLink = '';
+      this.deleteDisabled = true;
     }
     const selfRegistrationFormValue = deepClone(this.selfRegistrationParams);
     if (selfRegistrationFormValue.title?.length) {
