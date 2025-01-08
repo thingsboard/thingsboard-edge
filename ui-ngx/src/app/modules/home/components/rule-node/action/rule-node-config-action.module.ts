6fc6659cc77bfe1eb74bec0cab81841c079b8dba
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2025-01-08 13:05:35 +0200

    UI: Fixed license header

--- ui-ngx/src/app/modules/home/components/rule-node/action/rule-node-config-action.module.ts
+++ ui-ngx/src/app/modules/home/components/rule-node/action/rule-node-config-action.module.ts
@@ -1,17 +1,32 @@
 ///
-/// Copyright © 2016-2024 The Thingsboard Authors
+/// ThingsBoard, Inc. ("COMPANY") CONFIDENTIAL
 ///
-/// Licensed under the Apache License, Version 2.0 (the "License");
-/// you may not use this file except in compliance with the License.
-/// You may obtain a copy of the License at
+/// Copyright © 2016-2024 ThingsBoard, Inc. All Rights Reserved.
 ///
-///     http://www.apache.org/licenses/LICENSE-2.0
+/// NOTICE: All information contained herein is, and remains
+/// the property of ThingsBoard, Inc. and its suppliers,
+/// if any.  The intellectual and technical concepts contained
+/// herein are proprietary to ThingsBoard, Inc.
+/// and its suppliers and may be covered by U.S. and Foreign Patents,
+/// patents in process, and are protected by trade secret or copyright law.
 ///
-/// Unless required by applicable law or agreed to in writing, software
-/// distributed under the License is distributed on an "AS IS" BASIS,
-/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-/// See the License for the specific language governing permissions and
-/// limitations under the License.
+/// Dissemination of this information or reproduction of this material is strictly forbidden
+/// unless prior written permission is obtained from COMPANY.
+///
+/// Access to the source code contained herein is hereby forbidden to anyone except current COMPANY employees,
+/// managers or contractors who have executed Confidentiality and Non-disclosure agreements
+/// explicitly covering such access.
+///
+/// The copyright notice above does not evidence any actual or intended publication
+/// or disclosure  of  this source code, which includes
+/// information that is confidential and/or proprietary, and is a trade secret, of  COMPANY.
+/// ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC  PERFORMANCE,
+/// OR PUBLIC DISPLAY OF OR THROUGH USE  OF THIS  SOURCE CODE  WITHOUT
+/// THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED,
+/// AND IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES.
+/// THE RECEIPT OR POSSESSION OF THIS SOURCE CODE AND/OR RELATED INFORMATION
+/// DOES NOT CONVEY OR IMPLY ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS,
+/// OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
 ///
 
 import { NgModule, Type } from '@angular/core';
@@ -22,7 +37,6 @@ import { AttributesConfigComponent } from './attributes-config.component';
 import { TimeseriesConfigComponent } from './timeseries-config.component';
 import { RpcRequestConfigComponent } from './rpc-request-config.component';
 import { LogConfigComponent } from './log-config.component';
-import { AssignCustomerConfigComponent } from './assign-customer-config.component';
 import { ClearAlarmConfigComponent } from './clear-alarm-config.component';
 import { CreateAlarmConfigComponent } from './create-alarm-config.component';
 import { CreateRelationConfigComponent } from './create-relation-config.component';
@@ -34,7 +48,6 @@ import { MsgCountConfigComponent } from './msg-count-config.component';
 import { RpcReplyConfigComponent } from './rpc-reply-config.component';
 import { SaveToCustomTableConfigComponent } from './save-to-custom-table-config.component';
 import { RuleNodeConfigCommonModule } from '../common/rule-node-config-common.module';
-import { UnassignCustomerConfigComponent } from './unassign-customer-config.component';
 import { DeviceProfileConfigComponent } from './device-profile-config.component';
 import { PushToEdgeConfigComponent } from './push-to-edge-config.component';
 import { PushToCloudConfigComponent } from './push-to-cloud-config.component';
@@ -51,7 +64,6 @@ import { EmptyConfigComponent } from '@home/components/rule-node/empty-config.co
     TimeseriesConfigComponent,
     RpcRequestConfigComponent,
     LogConfigComponent,
-    AssignCustomerConfigComponent,
     ClearAlarmConfigComponent,
     CreateAlarmConfigComponent,
     CreateRelationConfigComponent,
@@ -62,7 +74,6 @@ import { EmptyConfigComponent } from '@home/components/rule-node/empty-config.co
     MsgCountConfigComponent,
     RpcReplyConfigComponent,
     SaveToCustomTableConfigComponent,
-    UnassignCustomerConfigComponent,
     SendRestApiCallReplyConfigComponent,
     DeviceProfileConfigComponent,
     PushToEdgeConfigComponent,
@@ -82,7 +93,6 @@ import { EmptyConfigComponent } from '@home/components/rule-node/empty-config.co
     TimeseriesConfigComponent,
     RpcRequestConfigComponent,
     LogConfigComponent,
-    AssignCustomerConfigComponent,
     ClearAlarmConfigComponent,
     CreateAlarmConfigComponent,
     CreateRelationConfigComponent,
@@ -93,7 +103,6 @@ import { EmptyConfigComponent } from '@home/components/rule-node/empty-config.co
     MsgCountConfigComponent,
     RpcReplyConfigComponent,
     SaveToCustomTableConfigComponent,
-    UnassignCustomerConfigComponent,
     SendRestApiCallReplyConfigComponent,
     DeviceProfileConfigComponent,
     PushToEdgeConfigComponent,
@@ -106,7 +115,6 @@ export class RuleNodeConfigActionModule {
 }
 
 export const ruleNodeActionConfigComponentsMap: Record<string, Type<IRuleNodeConfigurationComponent>> = {
-  'tbActionNodeAssignToCustomerConfig': AssignCustomerConfigComponent,
   'tbActionNodeAttributesConfig': AttributesConfigComponent,
   'tbActionNodeClearAlarmConfig': ClearAlarmConfigComponent,
   'tbActionNodeCreateAlarmConfig': CreateAlarmConfigComponent,
@@ -128,5 +136,4 @@ export const ruleNodeActionConfigComponentsMap: Record<string, Type<IRuleNodeCon
   'tbActionNodeCustomTableConfig': SaveToCustomTableConfigComponent,
   'tbActionNodeSendRestApiCallReplyConfig': SendRestApiCallReplyConfigComponent,
   'tbActionNodeTimeseriesConfig': TimeseriesConfigComponent,
-  'tbActionNodeUnAssignToCustomerConfig': UnassignCustomerConfigComponent
 };
